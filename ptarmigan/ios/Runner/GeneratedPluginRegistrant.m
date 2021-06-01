0a0f8fbe1db4cfb0056a0c69556c163f44ad7865
u17095230 <u17095230@tuks.co.za>
2021-06-01 21:26:42 +0200

    Basic File Structure Add
    Added flutter project skeleton.

---
+++ ptarmigan/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,47 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<amplify_api/AmplifyApiPlugin.h>)
+#import <amplify_api/AmplifyApiPlugin.h>
+#else
+@import amplify_api;
+#endif
+
+#if __has_include(<amplify_auth_cognito/AuthCognito.h>)
+#import <amplify_auth_cognito/AuthCognito.h>
+#else
+@import amplify_auth_cognito;
+#endif
+
+#if __has_include(<amplify_core/AmplifyCorePlugin.h>)
+#import <amplify_core/AmplifyCorePlugin.h>
+#else
+@import amplify_core;
+#endif
+
+#if __has_include(<amplify_datastore/AmplifyDataStorePlugin.h>)
+#import <amplify_datastore/AmplifyDataStorePlugin.h>
+#else
+@import amplify_datastore;
+#endif
+
+#if __has_include(<amplify_flutter/Amplify.h>)
+#import <amplify_flutter/Amplify.h>
+#else
+@import amplify_flutter;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AmplifyApiPlugin registerWithRegistrar:[registry registrarForPlugin:@"AmplifyApiPlugin"]];
+  [AuthCognito registerWithRegistrar:[registry registrarForPlugin:@"AuthCognito"]];
+  [AmplifyCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"AmplifyCorePlugin"]];
+  [AmplifyDataStorePlugin registerWithRegistrar:[registry registrarForPlugin:@"AmplifyDataStorePlugin"]];
+  [Amplify registerWithRegistrar:[registry registrarForPlugin:@"Amplify"]];
+}
+
+@end
