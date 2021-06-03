bffe2c99cf31bb42ea780d341307a73a9346f401
Reinier <u18136835@tuks.co.za>
2021-06-03 22:35:00 +0200

    Changed to work dynamodb

--- backend/sam/comprehend/comprehend/app.py
+++ backend/sam/comprehend/comprehend/app.py
@@ -3,35 +3,79 @@ import json
 # import requests
 import boto3
 
+
 def lambda_handler(event, context):
-    print(event)
-    event ={
-    "0": {
-        "Tweet Id": 1349868896208244739,
-        "Text": "Tesla trascura le donne",
-        "lang": "it",
-        "Weight": 1
-    }
-}
-    print("Some stuff happend")
+    'use strict'
+    # const
+    # AWS = require('aws-sdk');
+    #
+    # AWS.config.update({region: "eu-west-1"});
+    #
+    # exports.handler = async (event, context, callback) = > {
+    #     const
+    # ddb = new
+    # AWS.DynamoDB({apiVersion: "2012-10-08"});
+    # const
+    # documentClient = new
+    # AWS.DynamoDB.DocumentClient({region: "eu-west-1"});
+    #
+    # const
+    # params = {
+    #     TableName: "Twitter_Sentiment_Data",
+    #     Item: {
+    #         Tweet_Id: "202052523423",
+    #         Text: "Bitcoin is Trash",
+    #         lang: "en",
+    #         Weight: "1"
+    #
+    #     }
+    #
+    #     Item: {
+    #         Tweet_Id: "202052523423",
+    #         Text: "Bitcoin is Trash",
+    #         lang: "en",
+    #         Weight: "1"
+    #
+    #     }
+    # }
+    # try{
+    # const data = await documentClient.put(params).promise();
+    # console.log(data);
+    # }catch(err){
+    # console.log(err);
+    # }
+    #
+    # }
+
     # print(event)
-    supportedLanguage = ["ar", "hi", "ko", "zh-TW", "ja", "zh", "de", "pt", "en", "it", "fr", "es"]
+    dbClient = boto3.resource("dynamodb")
 
-    for item in event:
-        # print(event[item]["Text"])
-
-        if event[item]["lang"] in supportedLanguage:
-            # print("This shit is working")
-
-            comprehend = boto3.client("comprehend")
-            response = comprehend.detect_sentiment(Text =event[item]["Text"], LanguageCode = event[item]["lang"])
-            event[item]["sentement"] = response["Sentiment"]
-
-            # print(event[item])
-
-        else:
-            del event[item]
-
+    table = dbClient.Table('Twitter_Sentiment_Data')
+    table.put_item(
+        Item={
+            'Tweet_Id': "789456123",
+            'Text': "Benjamin is not great",
+            'lang': "en",
+            'Weight': "1"
+        })
+    # print(res)
+    # print(event)
+    # supportedLanguage = ["ar", "hi", "ko", "zh-TW", "ja", "zh", "de", "pt", "en", "it", "fr", "es"]
+    #
+    # for item in event:
+    #     # print(event[item]["Text"])
+    #
+    #     if event[item]["lang"] in supportedLanguage:
+    #         # print("This shit is working")
+    #
+    #         comprehend = boto3.client("comprehend")
+    #         response = comprehend.detect_sentiment(Text=event[item]["Text"], LanguageCode=event[item]["lang"])
+    #         event[item]["sentement"] = response["Sentiment"]
+    #
+    #         # print(event[item])
+    #
+    #     else:
+    #         del event[item]
 
     return {
         'statusCode': 200,
