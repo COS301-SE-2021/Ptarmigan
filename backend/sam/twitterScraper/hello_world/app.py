b662103e89ec6d6483ab65bfd63c78febc8c1881
Reinier <u18136835@tuks.co.za>
2021-06-04 02:54:46 +0200

    Tried to fix twitter scraper to work on SAM

--- backend/sam/twitterScraper/hello_world/app.py
+++ backend/sam/twitterScraper/hello_world/app.py
@@ -1,29 +1,50 @@
 import json
+import snscrape.modules.twitter as snt
+import pandas as pd
+
 
 # import requests
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    content = event['content']
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    tweets_list = []
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    # Using TwitterSearchScraper to scrape data and append tweets to list
+    # ["ar", "hi", "ko", "zh-TW", "ja", "zh", "de", "pt", "en", "it", "fr", "es"]
+    for i, tweet in enumerate(snt.TwitterSearchScraper(
+            content + " min_faves:100" + " lang:ar OR lang:hi OR lang:ko OR lang:zh OR lang:ja OR lang:de OR lang:pt OR lang:en OR lang:it OR lang:fr OR lang:es").get_items()):
+        if i >= 50:
+            break
+        tweets_list.append([tweet.content, tweet.retweetCount,
+                            tweet.likeCount, tweet.lang, tweet.date])
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+    # Creating a dataframe from the tweets list above
+    tweets_df = pd.DataFrame(tweets_list,
+                             columns=['Text', 'Retweets', 'Likes', 'lang', 'date'])
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+    # Weight Calc
+    weightList = []
+    for x in range(len(tweets_df)):
+        # Calculate Weight and append to weight list
+        weightList.append(1 + ((tweets_df.loc[x, 'Retweets']) * 3) + (tweets_df.loc[x, 'Likes']))
+    # add list to dataframe
+    tweets_df['Weight'] = weightList
 
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
+    # Remove unnecessary fields from table for output
+    tweets_df = tweets_df.drop(['Retweets', 'Likes'], axis=1)
 
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
+    jsonOutput = tweets_df.to_json(orient="index")
+    parsed = json.loads(jsonOutput)
+    # return json.dumps(parsed, indent=4)
+
+
+    scrapeThis = '{ "content": "Bitcoin"}'
+
+    print(json.dumps(parsed, indent=4))
+    print("some shit")
+
 
     # try:
     #     ip = requests.get("http://checkip.amazonaws.com/")
