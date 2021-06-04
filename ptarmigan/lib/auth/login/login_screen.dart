1c4737743c52966776d075d4a2a016242641cd47
L_Bradford <u18185208@tuks.co.za>
2021-06-04 02:58:47 +0200

    Update login_screen.dart

--- ptarmigan/lib/auth/login/login_screen.dart
+++ ptarmigan/lib/auth/login/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
@@ -8,12 +9,25 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  Future<String> _onLogin(BuildContext context, LoginData data) async {
+    print(data);
+    return "hi";
+  }
+
+  Future<String> _onSignup(BuildContext context, LoginData data) async {
+    try {
+      final res = await Amplify.Auth.signUp(
+          username: data.name, password: data.password);
+    } catch (e) {}
+    return "hi";
+  }
+
   @override
   Widget build(BuildContext context) {
     return FlutterLogin(
       title: "Ptarmigan",
-      onSignup: (_) => Future.value(''),
-      onLogin: (_) => Future.value(''),
+      onSignup: (LoginData data) => _onSignup(context, data),
+      onLogin: (LoginData data) => _onLogin(context, data),
       onRecoverPassword: (_) => Future.value(''),
       theme: LoginTheme(
         primaryColor: Theme.of(context).primaryColor,
