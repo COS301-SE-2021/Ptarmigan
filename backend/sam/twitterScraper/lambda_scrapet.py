f6f150c8c5f854f2b3f2130a6870731e1263756d
TylerArro <u18033882@tuks.co.za>
2021-06-04 02:01:09 +0200

    Updating unit test for scraper script

--- backend/sam/twitterScraper/lambda_scrapet.py
+++ backend/sam/twitterScraper/lambda_scrapet.py
@@ -1,25 +1,30 @@
 import json
-import snscrape.modules.twitter as snt
+import snscrape.modules.twitter
 import pandas as pd
 
 
-def scrapet_handler(event, context):
-    content = event['content']
-
+def return_tweet_list(content):
     tweets_list = []
-
     # Using TwitterSearchScraper to scrape data and append tweets to list
-    #["ar", "hi", "ko", "zh-TW", "ja", "zh", "de", "pt", "en", "it", "fr", "es"]
-    for i, tweet in enumerate(snt.TwitterSearchScraper(content + " min_faves:100" + " lang:ar OR lang:hi OR lang:ko OR lang:zh OR lang:ja OR lang:de OR lang:pt OR lang:en OR lang:it OR lang:fr OR lang:es").get_items()):
-        if i >= 50:
+    # ["ar", "hi", "ko", "zh-TW", "ja", "zh", "de", "pt", "en", "it", "fr", "es"]
+    for i, tweet in enumerate(snscrape.modules.twitter.TwitterSearchScraper(
+            content + " min_faves:100" + " lang:ar OR lang:hi OR lang:ko OR lang:zh OR lang:ja OR lang:de OR lang:pt OR lang:en OR lang:it OR lang:fr OR lang:es").get_items()):
+        if i >= 5:
             break
         tweets_list.append([tweet.content, tweet.retweetCount,
                             tweet.likeCount, tweet.lang, tweet.date])
 
     # Creating a dataframe from the tweets list above
+    return tweets_list
+
+
+def scrapet_handler(event, context):
+    content = event['content']
+    tweets_list = return_tweet_list(content)
     tweets_df = pd.DataFrame(tweets_list,
                              columns=['Text', 'Retweets', 'Likes', 'lang', 'date'])
 
+    # print (tweets_df.to_string)
     # Weight Calc
     weightList = []
     for x in range(len(tweets_df)):
@@ -28,7 +33,7 @@ def scrapet_handler(event, context):
     # add list to dataframe
     tweets_df['Weight'] = weightList
 
-    #Remove unnecessary fields from table for output
+    # Remove unnecessary fields from table for output
     tweets_df = tweets_df.drop(['Retweets', 'Likes'], axis=1)
 
     jsonOutput = tweets_df.to_json(orient="index")
--- backend/sam/twitterScraper/test_lambda_scrapet.py
+++ backend/sam/twitterScraper/test_lambda_scrapet.py
@@ -1,64 +1,34 @@
-import unittest
-from unittest import TestCase
-from unittest.mock import patch
-from unittest.mock import MagicMock
-import os
 import json
+from unittest import TestCase
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
+import pandas
 import snscrape.modules.twitter as snt
 import backend.sam.twitterScraper.lambda_scrapet as ls
 
-class TestClass(TestCase) :
 
-    def setUp(self) -> None:
-        self.test_tweet_context ='{ "content": "Bitcoin"}'
+class TestClass(TestCase):
 
-    @patch('snt.TwitterSearchScraper')
-    def test_IfScrapetReturnsTweetsWithCorrectData(self,mock_scrape):
+    def setUp(self):
+        self.test_tweet_context = '{"content": "Bitcoin"}'
+
+    @patch('backend.sam.twitterScraper.lambda_scrapet.return_tweet_df')
+    def test_IfScrapetReturnsTweetsWithCorrectData(self, mock_scrape):
         expected = ({
-    "0": {
-        "Text": "I'm interviewing @cameron &amp; @tyler at the Bitcoin Conference tomorrow.\n\nGemini is one of the most secure &amp; reliable crypto platforms. You can buy, sell, store, earn interest, or learn crypto - Gemini does it all. They even sponsor the podcast \ud83d\ude02\n\nSign up: https://t.co/WbdVHdxvH8",
-        "lang": "en",
-        "date": 1622747771000,
-        "Weight": 193
-    },
-    "1": {
-        "Text": "If you can\u2019t attend the conference, make sure to tune into the Day 1 livestream on YouTube:\n\nhttps://t.co/REqyW6sunm",
-        "lang": "en",
-        "date": 1622746176000,
-        "Weight": 203
-    },
-    "2": {
-        "Text": "I SAID BUY MORE  #BITCOIN #BitcoinConference https://t.co/VL7Yww6Fx2",
-        "lang": "en",
-        "date": 1622746104000,
-        "Weight": 2568
-    }
-    })
-
-        #mock data for scraper lbrary
-        mock_scrape.return_value = MagicMock({
-    "0": {
-        "Text": "I'm interviewing @cameron &amp; @tyler at the Bitcoin Conference tomorrow.\n\nGemini is one of the most secure &amp; reliable crypto platforms. You can buy, sell, store, earn interest, or learn crypto - Gemini does it all. They even sponsor the podcast \ud83d\ude02\n\nSign up: https://t.co/WbdVHdxvH8",
-        "Retweets": 8,
-        "Likes": 168,
-        "lang": "en",
-        "date": 1622747771000
-    },
-    "1": {
-        "Text": "If you can\u2019t attend the conference, make sure to tune into the Day 1 livestream on YouTube:\n\nhttps://t.co/REqyW6sunm",
-        "Retweets": 25,
-        "Likes": 127,
-        "lang": "en",
-        "date": 1622746176000
-    },
-    "2": {
-        "Text": "I SAID BUY MORE  #BITCOIN #BitcoinConference https://t.co/VL7Yww6Fx2",
-        "Retweets": 184,
-        "Likes": 3016,
-        "lang": "en",
-        "date": 1622746104000
-    }
-})
-        testreturn = (ls.scrapet_handler(json.loads(self.test_tweet_context),""))
-        assert json.dumps(testreturn) == expected
-
+            "0": {
+                "Text": "I SAID BUY MORE  #BITCOIN #BitcoinConference https://t.co/VL7Yww6Fx2",
+                "lang": "en",
+                "date": 1622746104000,
+                "Weight": 2568
+            }
+        })
+        #expected = '{}'
+        # mock data for scraper lbrary
+        mock_scrape.return_value = pandas.DataFrame()
+        testReturn = (ls.scrapet_handler(json.loads(self.test_tweet_context), ""))
+        #print(testReturn)
+        #print(expected)
+        #print(type(expected))
+        #print(type(testReturn))
+        assert testReturn == expected
