d37d2e73f63d72c0a699f47907fff3ba255ea982
TylerArro <u18033882@tuks.co.za>
2021-06-10 15:31:53 +0200

    Optomized imports for scraper script and its test files

--- backend/sam/twitterScraper/intergrationtest_lambda_scrapet.py
+++ backend/sam/twitterScraper/intergrationtest_lambda_scrapet.py
@@ -7,11 +7,12 @@ import backend.sam.twitterScraper.lambda_scrapet as ls
 class TestClass(TestCase):
 
     def setUp(self):
-        self.test_tweet_context = '{"content": "Bitcoin"}'
+        self.test_tweet_context = '{"content": "Tesla"}'
 
     def test_scraper_returns_tweets(self):
         testreturn = (ls.scrapet_handler(json.loads(self.test_tweet_context), ""))
-        print(testreturn)
+
+        #print(testreturn)
         #check the function returns a string
         assert isinstance(testreturn,str)
         #turn the string back into dict
--- backend/sam/twitterScraper/lambda_scrapet.py
+++ backend/sam/twitterScraper/lambda_scrapet.py
@@ -9,19 +9,20 @@ def return_tweet_list(content):
     # ["ar", "hi", "ko", "zh-TW", "ja", "zh", "de", "pt", "en", "it", "fr", "es"]
     for i, tweet in enumerate(snscrape.modules.twitter.TwitterSearchScraper(
             content + " min_faves:100" + " lang:ar OR lang:hi OR lang:ko OR lang:zh OR lang:ja OR lang:de OR lang:pt OR lang:en OR lang:it OR lang:fr OR lang:es").get_items()):
-        if i >= 30:
+        if i >= 100:
             break
-        tweets_list.append([tweet.content, tweet.retweetCount,
+        tweets_list.append([tweet.id, tweet.content, tweet.retweetCount,
                             tweet.likeCount, tweet.lang, tweet.date])
     # Creating a dataframe from the tweets list above
     return tweets_list
 
-
 def scrapet_handler(event, context):
     content = event['content']
+    #call helper function
     tweets_list = return_tweet_list(content)
+    #create dataframe
     tweets_df = pd.DataFrame(tweets_list,
-                             columns=['Text', 'Retweets', 'Likes', 'lang', 'date'])
+                             columns=['Tweet Id', 'Text', 'Retweets', 'Likes', 'lang', 'date'])
 
     # print (tweets_df.to_string)
     # Weight Calc
@@ -38,3 +39,4 @@ def scrapet_handler(event, context):
     jsonOutput = tweets_df.to_json(orient="index")
     parsed = json.loads(jsonOutput)
     return json.dumps(parsed, indent=4)
+
