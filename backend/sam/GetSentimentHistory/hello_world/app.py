d1a072d980251e84170618f3a511590704464b61
Reinier <u18136835@tuks.co.za>
2021-06-11 19:46:39 +0200

    Added calculations

--- backend/sam/GetSentimentHistory/hello_world/app.py
+++ backend/sam/GetSentimentHistory/hello_world/app.py
@@ -6,6 +6,23 @@ from boto3.dynamodb.conditions import Key
 import json
 
 # import requests
+def calculateSentiment(content):
+    totalVotes = 0
+    runningSentiment = 0
+    for element in content:
+        weight = int(element["Weight"])
+        totalVotes = totalVotes + weight
+        sentiment = 0
+        if element["Sentiment"] == "NEUTRAL":
+            sentiment = 0
+        if element["Sentiment"] == "POSITIVE":
+            sentiment = 1
+        if element["Sentiment"] == "NEGATIVE":
+            sentiment = -1
+
+        runningSentiment = runningSentiment + (weight * sentiment)
+
+    return (runningSentiment/totalVotes)
 
 
 def lambda_handler(event, context):
@@ -14,7 +31,10 @@ def lambda_handler(event, context):
     response = table.scan(
         FilterExpression = Key('Tweet_Id').gt(50) & Key('TimeStamp').gt(1))
 
-    print(response["Items"])
+    # print(response["Items"])
+
+
+    print(calculateSentiment(response["Items"]))
 
     return {
         "statusCode": 200,
