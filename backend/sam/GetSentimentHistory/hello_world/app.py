bdb21c37f6fe1d7fa58ae5bfa308b9047b60d6ae
Reinier <u18136835@tuks.co.za>
2021-06-11 17:27:33 +0200

    Test if database read is working

--- backend/sam/GetSentimentHistory/hello_world/app.py
+++ backend/sam/GetSentimentHistory/hello_world/app.py
@@ -1,14 +1,20 @@
 import json
 from pprint import pprint
 import boto3
-from boto3 import dynamodb
 from botocore.exceptions import ClientError
 
 # import requests
 
 
 def lambda_handler(event, context):
-    table = dynamodb.Table('Movies')
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Twitter_Sentiment_Data')
+    response = table.get_item(Key = {
+        "Tweet_Id" : "999"
+
+    })
+
+    print(response)
 
     return {
         "statusCode": 200,
