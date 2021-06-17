813a8afe0e32a3b7f9ece9a173d796bd589df241
Reinier <u18136835@tuks.co.za>
2021-06-17 22:31:03 +0200

    Added Test parameters

--- backend/sam/get_SentimentGraph/tests/unit/test_handler.py
+++ backend/sam/get_SentimentGraph/tests/unit/test_handler.py
@@ -7,11 +7,20 @@ from hello_world import app
 
 class TestClass(TestCase):
     def setUp(self):
-        self.test_context = "some"
+        self.test_context = {
+                              "BeginDate" : 1623008136000,
+                              "Interval":  "Week",
+                              "CompanyName": "Tesla"
+                            }
         # Input data
 
-    def get_sentiment_graph(self):
-        testReturn = (app.lambda_handler(json.loads(self.test_context)), json.dumps("{}"))
+    def test_get_sentiment_graph(self):
+        testReturn = (app.lambda_handler(self.test_context, json.dumps("{}")))
         print("some")
 
-        assert isinstance(testReturn, str)
+        expected = {"statusCode": 200,
+                             "body": {"CompanyName": "Tesla", "Interval": 604800000, "Data": [
+            {"BeginDate": 1623008136000, "EndDate": 1623612936000, "IntervalData": 0},
+            {"BeginDate": 1623612936000, "EndDate": 1624217736000, "IntervalData": 0}]}}
+
+        assert isinstance(testReturn)
