134d83c448705c01de925af54a237d83b0d1fe53
Reinier <u18136835@tuks.co.za>
2021-06-17 22:16:23 +0200

    Added test body

--- backend/sam/get_SentimentGraph/tests/unit/test_handler.py
+++ backend/sam/get_SentimentGraph/tests/unit/test_handler.py
@@ -7,6 +7,11 @@ from hello_world import app
 
 class TestClass(TestCase):
     def setUp(self):
+        self.test_context = "some"
         # Input data
 
     def get_sentiment_graph(self):
+        testReturn = (app.lambda_handler(json.loads(self.test_context)), json.dumps("{}"))
+        print("some")
+
+        assert isinstance(testReturn, str)
