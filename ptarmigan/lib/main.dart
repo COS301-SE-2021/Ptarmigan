2380b6c9a9fc921a4f6479676ac905e66f51938f
L_Bradford <u18185208@tuks.co.za>
2021-06-18 12:56:36 +0200

    Implemented basic popup notification screen

--- ptarmigan/lib/main.dart
+++ ptarmigan/lib/main.dart
@@ -7,12 +7,13 @@ import 'package:flutter/material.dart';
 // amplify packages we will need to use
 import 'package:amplify_flutter/amplify.dart';
 import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'package:ptarmigan/entry.dart';
 import 'package:ptarmigan/screens/dashboard.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 // amplify configuration and models that should have been generated for you
-import 'amplifyconfiguration.dart';
+//import 'amplifyconfiguration.dart';
 import 'auth/flutter_login/login_screen.dart';
 import 'auth/login/confirm.dart';
 import 'models/ModelProvider.dart';
@@ -21,366 +22,64 @@ import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp((MaterialApp(home: MyApp())));
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Amplified Todo',
-      //home: TodosPage(),
-      //home: Login(),
-      //home: EntryScreen(),
-      onGenerateRoute: (settings) {
-        if (settings.name == '/confirm') {
-          return PageRouteBuilder(
-            pageBuilder: (_, __, ___) =>
-                ConfirmScreen(data: settings.arguments as LoginData),
-            transitionsBuilder: (_, __, ___, child) => child,
-          );
-        }
-        if (settings.name == '/dashboard') {
-          return PageRouteBuilder(
-            pageBuilder: (_, __, ___) => DashboardScreen(),
-            transitionsBuilder: (_, __, ___, child) => child,
-          );
-        }
-        return MaterialPageRoute(builder: (_) => EntryScreen());
-      },
-    );
-  }
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
 }
 
-class TodosPage extends StatefulWidget {
-  @override
-  _TodosPageState createState() => _TodosPageState();
-}
-
-class _TodosPageState extends State<TodosPage> {
-  bool _isLoading;
-  bool isSignUpComplete;
-  StreamSubscription _subscription;
-  List<Todo> _todos;
-
-  final AmplifyDataStore _dataStorePlugin =
-      AmplifyDataStore(modelProvider: ModelProvider.instance);
-
-  final AmplifyAPI _apiPlugin = AmplifyAPI();
-  final AmplifyAuthCognito _authPlugin = AmplifyAuthCognito();
+class _MyAppState extends State<MyApp> {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
   @override
   void initState() {
-    _isLoading = true;
-    _todos = [];
-    try {
-      Amplify.Auth.signOut();
-    } catch (e) {}
-    _initializeApp();
     super.initState();
+    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    var android = new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iOS = new IOSInitializationSettings();
+    var initSetttings = new InitializationSettings(android: android, iOS: iOS);
+    flutterLocalNotificationsPlugin.initialize(initSetttings,
+        onSelectNotification: onSelectNotification);
   }
 
-  @override
-  void dispose() {
-    // cancel the subscription when the state is removed from the tree
-    _subscription.cancel();
-    super.dispose();
-  }
-
-  Future<void> _initializeApp() async {
-    // configure Amplify
-    //await _configureAmplify();                              ///////////Remove if other configure doesnt work
-
-    // listen for updates to Todo entries by passing the Todo classType to
-    // Amplify.DataStore.observe() and when an update event occurs, fetch the
-    // todo list
-    //
-    // note this strategy may not scale well with larger number of entries
-    _subscription = Amplify.DataStore.observe(Todo.classType).listen((event) {
-      _fetchTodos();
-    });
-
-    // fetch Todo entries from DataStore
-    await _fetchTodos();
-
-    // after both configuring Amplify and fetching Todo entries, update loading
-    // ui state to loaded state
-    setState(() {
-      _isLoading = false;
-    });
-  }
-
-  //---------------------------------------------------------------------
-  //Beginning of signing in flow
-
-  Future<void> _configureAmplify() async {
-    try {
-      // add Amplify plugins
-      //await Amplify.addPlugins([_dataStorePlugin]);
-
-      print('----------WRONG CONFIGURE RUNNING -------------');
-      await Amplify.addPlugins([
-        _dataStorePlugin,
-        _apiPlugin,
-        _authPlugin,
-      ]);
-      // configure Amplify
-      //
-      // note that Amplify cannot be configured more than once!
-      await Amplify.configure(amplifyconfig);
-    } catch (e) {
-      // error handling can be improved for sure!
-      // but this will be sufficient for the purposes of this tutorial
-      print('An error occurred while configuring Amplify: $e');
-    }
-  }
-
-  Future<void> _fetchTodos() async {
-    try {
-      // query for all Todo entries by passing the Todo classType to
-      // Amplify.DataStore.query()
-      List<Todo> updatedTodos = await Amplify.DataStore.query(Todo.classType);
-
-      // update the ui state to reflect fetched todos
-      setState(() {
-        _todos = updatedTodos;
-      });
-    } catch (e) {
-      print('An error occurred while querying Todos: $e');
-    }
+  Future onSelectNotification(String payload) {
+    debugPrint("payload : $payload");
+    showDialog(
+      context: context,
+      builder: (_) => new AlertDialog(
+        title: new Text('Notification'),
+        content: new Text('$payload'),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Feeds'),
-        backgroundColor: Color(0xFFE27D21),
+      appBar: new AppBar(
+        title: new Text('Lukes amazing pop up notification'),
       ),
-      //body: Center(child: CircularProgressIndicator()),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : TodosList(todos: _todos),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AddTodoForm()),
-          );
-        },
-        tooltip: 'Add insight',
-        label: Row(
-          children: [Icon(Icons.add), Text('Add insight')],
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
-  }
-}
-
-class TodosList extends StatelessWidget {
-  final List<Todo> todos;
-  final pageViewController = PageController();
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-
-  TodosList({this.todos});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      body: Container(
-        width: double.infinity,
-        height: 500,
-        child: Stack(
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
-              child: PageView(
-                controller: pageViewController,
-                scrollDirection: Axis.horizontal,
-                children: [
-                  todos.length >= 1
-                      ? ListView(
-                          padding: EdgeInsets.zero,
-                          scrollDirection: Axis.vertical,
-                          children: todos
-                              .map((todo) => TodoItem(todo: todo))
-                              .toList())
-                      : Center(child: Text('Tap button below to add a todo!')),
-                  ListView(
-                    padding: EdgeInsets.zero,
-                    scrollDirection: Axis.vertical,
-                    children: [],
-                  ),
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment(0, 1),
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                child: SmoothPageIndicator(
-                  controller: pageViewController,
-                  count: 2,
-                  axisDirection: Axis.horizontal,
-                  onDotClicked: (i) {
-                    pageViewController.animateToPage(
-                      i,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.ease,
-                    );
-                  },
-                  effect: ExpandingDotsEffect(
-                    expansionFactor: 2,
-                    spacing: 8,
-                    radius: 16,
-                    dotWidth: 16,
-                    dotHeight: 16,
-                    dotColor: Color(0xFF9E9E9E),
-                    activeDotColor: Color(0xFFE27D21),
-                    paintStyle: PaintingStyle.fill,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-
-    /*
-    
-
-    PageView(
-        controller: pageViewController,
-        scrollDirection: Axis.horizontal,
-        children: [
-         
-        ]); */
-  }
-}
-
-class TodoItem extends StatelessWidget {
-  final double iconSize = 24.0;
-  final Todo todo;
-
-  TodoItem({this.todo});
-
-  void _deleteTodo(BuildContext context) async {
-    try {
-      // to delete data from DataStore, we pass the model instance to
-      // Amplify.DataStore.delete()
-      await Amplify.DataStore.delete(todo);
-    } catch (e) {
-      print('An error occurred while deleting Todo: $e');
-    }
-  }
-
-  Future<void> _toggleIsComplete() async {
-    // copy the Todo we wish to update, but with updated properties
-    Todo updatedTodo = todo.copyWith(isComplete: !todo.isComplete);
-    try {
-      // to update data in DataStore, we again pass an instance of a model to
-      // Amplify.DataStore.save()
-      await Amplify.DataStore.save(updatedTodo);
-    } catch (e) {
-      print('An error occurred while saving Todo: $e');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: InkWell(
-        onTap: () {
-          _toggleIsComplete();
-        },
-        onLongPress: () {
-          _deleteTodo(context);
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(todo.name,
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  Text(todo.description ?? 'No description'),
-                ],
-              ),
-            ),
-            Icon(
-                todo.isComplete
-                    ? Icons.check_box
-                    : Icons.check_box_outline_blank,
-                size: iconSize),
-          ]),
-        ),
-      ),
-    );
-  }
-}
-
-class AddTodoForm extends StatefulWidget {
-  @override
-  _AddTodoFormState createState() => _AddTodoFormState();
-}
-
-class _AddTodoFormState extends State<AddTodoForm> {
-  final _nameController = TextEditingController();
-  final _descriptionController = TextEditingController();
-
-  Future<void> _saveTodo() async {
-    // get the current text field contents
-    String name = _nameController.text;
-    String description = _descriptionController.text;
-
-    // create a new Todo from the form values
-    // `isComplete` is also required, but should start false in a new Todo
-    Todo newTodo = Todo(
-        name: name,
-        description: description.isNotEmpty ? description : null,
-        isComplete: false);
-
-    try {
-      // to write data to DataStore, we simply pass an instance of a model to
-      // Amplify.DataStore.save()
-      await Amplify.DataStore.save(newTodo);
-      // after creating a new Todo, close the form
-      Navigator.of(context).pop();
-    } catch (e) {
-      print('An error occurred while saving Todo: $e');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Add Feed'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                  controller: _nameController,
-                  decoration: InputDecoration(filled: true, labelText: 'Name')),
-              TextFormField(
-                  controller: _descriptionController,
-                  decoration:
-                      InputDecoration(filled: true, labelText: 'Description')),
-              ElevatedButton(onPressed: _saveTodo, child: Text('Save'))
-            ],
+      body: new Center(
+        child: new ElevatedButton(
+          onPressed: showNotification,
+          child: new Text(
+            'Tap to have your mind blown',
+            style: Theme.of(context).textTheme.headline5,
           ),
         ),
       ),
     );
+  }
+
+  showNotification() async {
+    var android = new AndroidNotificationDetails(
+        'channel id', 'channel NAME', 'CHANNEL DESCRIPTION',
+        priority: Priority.high, importance: Importance.max);
+    var iOS = new IOSNotificationDetails();
+    var platform = new NotificationDetails(android: android, iOS: iOS);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'BOOOOM', 'MIND = BLOWN', platform,
+        payload: 'AndroidCoding.in');
   }
 }
