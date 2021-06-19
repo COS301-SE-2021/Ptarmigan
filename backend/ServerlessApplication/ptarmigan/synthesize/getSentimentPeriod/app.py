16f938bd6dd5aa265261c004c692b06f61bce364
Reinier <u18136835@tuks.co.za>
2021-06-19 22:38:37 +0200

    Added Get sentiment period function

---
+++ backend/ServerlessApplication/ptarmigan/synthesize/getSentimentPeriod/app.py
@@ -0,0 +1,58 @@
+import json
+from pprint import pprint
+import boto3
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key
+import json
+
+# import requests
+def calculateSentiment(content):
+    totalVotes = 0
+    runningSentiment = 0
+    for element in content:
+        weight = int(element["Weight"])
+        totalVotes = totalVotes + weight
+        sentiment = 0
+        if element["Sentiment"] == "NEUTRAL":
+            sentiment = 0
+        if element["Sentiment"] == "POSITIVE":
+            sentiment = 1
+        if element["Sentiment"] == "NEGATIVE":
+            sentiment = -1
+
+        runningSentiment = runningSentiment + (weight * sentiment)
+
+    return (runningSentiment/totalVotes)
+
+
+def lambda_handler(event, context):
+
+    print(event)
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Test')
+    try:
+        response = table.scan(
+            FilterExpression = Key('Tweet_Id').gt(1) & Key('TimeStamp').between(event["BeginDate"], event["EndDate"])& Key('CompanyName').eq(event["CompanyName"])
+        )
+
+        calculation = calculateSentiment(response["Items"])
+
+        return {
+            "statusCode": 200,
+            "body": calculation
+        }
+
+    except:
+        return {
+            "statusCode": 400,
+            "body": "Parameters not found in DB"
+        }
+
+
+# //{
+#   "BeginDate" : 1623005418000,
+#   "Interval":  "Day",
+#   "CompanyName": "Tesla"
+# }
+
+
