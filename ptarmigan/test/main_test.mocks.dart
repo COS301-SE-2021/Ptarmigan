fe66ae31e840301ce942cf874acf667a8d6e4735
L_Bradford <u18185208@tuks.co.za>
2021-06-21 01:31:37 +0200

    Update main_test.mocks.dart

--- ptarmigan/test/main_test.mocks.dart
+++ ptarmigan/test/main_test.mocks.dart
@@ -3,8 +3,9 @@
 // Do not manually edit this file.
 
 import 'package:mockito/mockito.dart' as _i1;
-import 'package:ptarmigan/main.dart' as _i2;
+//import 'package:ptarmigan/main.dart' as _i2;
 import 'package:ptarmigan/models/Feed.dart' as _i3;
+import 'package:ptarmigan/services/feed_changer.dart' as i2;
 
 // ignore_for_file: avoid_redundant_argument_values
 // ignore_for_file: comment_references
@@ -15,15 +16,15 @@ import 'package:ptarmigan/models/Feed.dart' as _i3;
 /// A class which mocks [FeedChanger].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockFeedChanger extends _i1.Mock implements _i2.FeedChanger {}
+//class MockFeedChanger extends _i1.Mock implements _i2.FeedChanger {}
 
 /// A class which mocks [Subscriber].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockSubscriber extends _i1.Mock implements _i2.Subscriber {}
+//class MockSubscriber extends _i1.Mock implements _i2.Subscriber {}
 
 /// A class which mocks [Feed].
 ///
 /// See the documentation for Mockito's code generation for more information.
 // ignore: must_be_immutable
-class MockFeed extends _i1.Mock implements _i3.Feed {}
+//class MockFeed extends _i1.Mock implements _i3.Feed {}
