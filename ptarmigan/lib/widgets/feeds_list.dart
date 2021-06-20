3562d0cd72039d198f4d99fe60a73c5d8f70299a
L_Bradford <u18185208@tuks.co.za>
2021-06-21 01:31:20 +0200

    Create feeds_list.dart

---
+++ ptarmigan/lib/widgets/feeds_list.dart
@@ -0,0 +1,91 @@
+// @dart=2.9
+
+import 'dart:async';
+// flutter and ui libraries
+import 'package:amplify_auth_cognito/method_channel_auth_cognito.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+// amplify packages we will need to use
+import 'package:amplify_flutter/amplify.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:provider/provider.dart';
+import 'package:ptarmigan/models/ModelProvider.dart';
+import 'package:ptarmigan/widgets/add_feed_form.dart';
+import 'package:ptarmigan/widgets/feed_items.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+// amplify configuration and models that should have been generated for you
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+
+class FeedsList extends StatelessWidget {
+  final List<Todo> todos;
+  final List<Feed> feeds;
+  final List<Feed> feedsSub;
+  final pageViewController = PageController();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final String title;
+  FeedsList({this.todos, this.feeds,this.feedsSub, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 280,
+      height: double.infinity,
+      color: Colors.white,
+      child: Stack(
+        children: [
+          Container(
+              child: feeds.length >= 1
+                  ? ListView(
+                      padding: EdgeInsets.fromLTRB(0, 229, 0, 0),
+                      scrollDirection: Axis.vertical,
+                      children:
+                          feeds.map((feeds) => FeedItems(feed: feeds)).toList())
+                  : Center(child: Text(''))),
+          DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.tealAccent,
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(0, 2, 30, 0),
+                      child: Text(
+                        'Your Feeds',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Montserrat"),
+                      )),
+                  Container(
+                      child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 160, 0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => AddFeedForm(feedsSub)),
+                        );
+                      },
+                      child: Text(
+                        'Add Feed',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )),
+                ],
+              )),
+        ],
+      ),
+    );
+    /*
+    PageView(
+        controller: pageViewController,
+        scrollDirection: Axis.horizontal,
+        children: [
+         
+        ]); */
+  }
+}
