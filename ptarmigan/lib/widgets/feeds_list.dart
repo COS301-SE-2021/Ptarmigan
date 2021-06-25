6eb44b7e6917b05d00d7f11aa6f56d5cf2f8f079
u17095230 <u17095230@tuks.co.za>
2021-06-25 15:50:19 +0200

    Rough Graph Added
    A rough graph to display sentiment has been added. It is not near its final state

--- ptarmigan/lib/widgets/feeds_list.dart
+++ ptarmigan/lib/widgets/feeds_list.dart
@@ -12,6 +12,7 @@ import 'package:provider/provider.dart';
 import 'package:ptarmigan/models/ModelProvider.dart';
 import 'package:ptarmigan/widgets/add_feed_form.dart';
 import 'package:ptarmigan/widgets/feed_items.dart';
+import 'package:ptarmigan/widgets/graph.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 // amplify configuration and models that should have been generated for you
 import 'package:amplify_api/amplify_api.dart';
@@ -47,45 +48,56 @@ class FeedsList extends StatelessWidget {
                           .map((feedsSub) => FeedItems(feed: feedsSub))
                           .toList())
                   : Center(child: Text(''))),
-          
-             DrawerHeader(
-                decoration: BoxDecoration(
-                 color: Colors.tealAccent,
-                ),
-               
-                
-                child: Column(
-                  key: Key("ColumnOne"),
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.fromLTRB(0, 2, 30, 0),
-                        child: Text(
-                          'Your Feeds',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              fontSize: 35,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: "Montserrat"),
-                        )),
-                    Container(
-                        child: Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 160, 0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      AddFeedForm(feeds: feeds)));
-                        },
-                        child: Text(
-                          'Add Feed',
-                          textAlign: TextAlign.center,
-                        ),
+          DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.tealAccent,
+              ),
+              child: Column(
+                key: Key("ColumnOne"),
+                children: [
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(0, 2, 30, 0),
+                      child: Text(
+                        'Your Feeds',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Montserrat"),
+                      )),
+                  Container(
+                      child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 160, 0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    AddFeedForm(feeds: feeds)));
+                      },
+                      child: Text(
+                        'Add Feed',
+                        textAlign: TextAlign.center,
                       ),
-                    )),
-                  ],
-                )),
+                    ),
+                  )),
+                  Container(
+                      child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 180, 0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Graph()));
+                      },
+                      child: Text(
+                        'Graph',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )),
+                ],
+              )),
         ],
       ),
     );
