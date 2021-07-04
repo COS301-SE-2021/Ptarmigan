c51d85bb6e5f6c56d810742b085d2e80b61f34e3
TylerArro <u18033882@tuks.co.za>
2021-07-04 18:27:24 +0200

    Updated lambda function for s3 bucket file

--- backend/sam/BucketInput/lambda_function.py
+++ backend/sam/BucketInput/lambda_function.py
@@ -1,8 +1,16 @@
 import json
-
+import boto3
 
 def lambda_handler(event, context):
+    s3client = boto3.client('s3')
 
-    # TODO implement
+    bucketname = 'stepfunctestbucket'
+    file_to_read = 'scrapeContent.json'
+
+    fileobj = s3client.get_object(
+        Bucket=bucketname,
+        Key=file_to_read
+    )
+
     return "nothing"
 
