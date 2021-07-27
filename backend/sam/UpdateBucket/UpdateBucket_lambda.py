20aed6ad5dc985ad0a253b0e978857eb7ccc3609
TylerArro <u18033882@tuks.co.za>
2021-07-27 02:52:15 +0200

    Updated scraper to work with step function

--- backend/sam/UpdateBucket/UpdateBucket_lambda.py
+++ backend/sam/UpdateBucket/UpdateBucket_lambda.py
@@ -6,6 +6,8 @@ def lambda_handler(event, context):
     update = event['content']
     print(update)
     s3client = boto3.client('s3')
+    bucketname = 'stepfunctestbucket'
+    file_to_read = 'scrapeContent.json'
     fileobj = s3client.get_object(
         Bucket=bucketname,
         Key=file_to_read
