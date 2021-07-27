4120c75fb1ebb09524815900d2d9596810d35c1f
Reinier <u18136835@tuks.co.za>
2021-07-27 16:28:35 +0200

    Linked the db

--- backend/ServerlessApplication/ptarmigan/synthesize/getSentimentPeriod/app.py
+++ backend/ServerlessApplication/ptarmigan/synthesize/getSentimentPeriod/app.py
@@ -26,7 +26,7 @@ def calculateSentiment(content):
 
 def dbReturn(event):
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('Test')
+    table = dynamodb.Table(TestOne)
 
     response = table.scan(
         FilterExpression=Key('Tweet_Id').gt(1) & Key('TimeStamp').between(event["BeginDate"], event["EndDate"]) & Key(
