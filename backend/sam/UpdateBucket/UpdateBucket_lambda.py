77db8939271b543296045e30fc4c407f9cd096c0
TylerArro <u18033882@tuks.co.za>
2021-07-27 04:34:14 +0200

    updating update bucket function

--- backend/sam/UpdateBucket/UpdateBucket_lambda.py
+++ backend/sam/UpdateBucket/UpdateBucket_lambda.py
@@ -2,8 +2,11 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    # read file
+
+    # read context from passed json argument
     update = event['content']
+
+    #connet to s3 and file the scrape conent file
     s3client = boto3.client('s3')
     bucketname = 'stepfunctestbucket'
     file_to_read = 'scrapeContent.json'
@@ -12,11 +15,12 @@ def lambda_handler(event, context):
         Key=file_to_read
     )
 
-    # write to file
+    # decode file into python dict
     filedata = fileobj['Body'].read()
     filecontents = (filedata.decode('utf-8'))
     filecontents = json.loads(filecontents)
 
+    #append content onto the dict
     replaceContent = filecontents['scrape-detail']
     replaceLine = '{"content": "' + update + '"}'
     replaceLine = json.loads(replaceLine)
@@ -24,8 +28,10 @@ def lambda_handler(event, context):
 
     filecontents['scrape-detail'] = replaceContent
 
+    #encode python dict to bytes for upload
     uploadByteStream = bytes(json.dumps(filecontents).encode('UTF-8'))
 
+    #try upload return error is failed
     try:
         s3client.put_object(
             Bucket=bucketname,
@@ -36,7 +42,7 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps('Error updating Content file')
         }
-
+    #else return success json
     return {
         'statusCode': 200,
         'body': json.dumps('Successfully Updated the Content to scrap.')
