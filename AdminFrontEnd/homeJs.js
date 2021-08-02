d6a7eeff81231f631de7e92af88ff373bc48fc56
Reinier <u18136835@tuks.co.za>
2021-08-02 15:24:32 +0200

    Remove table row on click

--- AdminFrontEnd/homeJs.js
+++ AdminFrontEnd/homeJs.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
     $('.completedTask').click(function () {
-        if($(this).text() == 'Yes') {
-            $(this).text('No');
-        }
-        else {
-            $(this).text('Yes');
-        }
+        $(this).parent().parent().remove()
     })
 });
