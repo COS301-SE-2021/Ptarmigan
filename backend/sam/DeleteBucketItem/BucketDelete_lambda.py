bd4a6cff537f7f73423186093a847b29da920d65
TylerArro <u18033882@tuks.co.za>
2021-08-05 10:48:36 +0200

    Updated Delete Bucket Item lambda function

--- backend/sam/DeleteBucketItem/BucketDelete_lambda.py
+++ backend/sam/DeleteBucketItem/BucketDelete_lambda.py
@@ -3,6 +3,21 @@ import boto3
 
 def lambda_handler(event, context):
     toDelete = event['delete']
+
+    s3client = boto3.client('s3')
+
+    bucketname = 'stepfunctestbucket'
+    file_to_read = 'scrapeContent.json'
+
+    fileobj = s3client.get_object(
+        Bucket=bucketname,
+        Key=file_to_read
+    )
+
+    filedata = fileobj['Body'].read()
+    filecontents = (filedata.decode('utf-8'))
+    filecontents = json.loads(filecontents)
+
     return {
         'statusCode': 200,
         'body': json.dumps('Item Deleted successfully and Content file has been updated')
