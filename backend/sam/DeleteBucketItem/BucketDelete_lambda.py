108216933bb8778a8db5505e3259f9fe2fc0ce4a
TylerArro <u18033882@tuks.co.za>
2021-08-05 10:49:43 +0200

    Updated Delete Bucket Item lambda function

--- backend/sam/DeleteBucketItem/BucketDelete_lambda.py
+++ backend/sam/DeleteBucketItem/BucketDelete_lambda.py
@@ -30,6 +30,17 @@ def lambda_handler(event, context):
 
     uploadByteStream = bytes(json.dumps(filecontents).encode('UTF-8'))
 
+    try:
+        s3client.put_object(
+            Bucket=bucketname,
+            Key=file_to_read,
+            Body=uploadByteStream)
+    except:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error updating Content file')
+        }
+
     return {
         'statusCode': 200,
         'body': json.dumps('Item Deleted successfully and Content file has been updated')
