86ad53b8d77dd9e6fae189bc3486fe61a20f9168
TylerArro <u18033882@tuks.co.za>
2021-08-05 10:50:23 +0200

    Updated Delete Bucket Item lambda function

--- backend/sam/DeleteBucketItem/BucketDelete_lambda.py
+++ backend/sam/DeleteBucketItem/BucketDelete_lambda.py
@@ -41,7 +41,13 @@ def lambda_handler(event, context):
             'body': json.dumps('Error updating Content file')
         }
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Item Deleted successfully and Content file has been updated')
-    }
+    if delFlag == True:
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Item Deleted successfully and Content file has been updated')
+        }
+    else:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Item not found.')
+        }
