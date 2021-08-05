797939d50613d6fcd7294356165cca5c6b38dc7f
TylerArro <u18033882@tuks.co.za>
2021-08-05 10:49:06 +0200

    Updated Delete Bucket Item lambda function

--- backend/sam/DeleteBucketItem/BucketDelete_lambda.py
+++ backend/sam/DeleteBucketItem/BucketDelete_lambda.py
@@ -18,6 +18,12 @@ def lambda_handler(event, context):
     filecontents = (filedata.decode('utf-8'))
     filecontents = json.loads(filecontents)
 
+    replaceContent = filecontents['scrape-detail']
+
+    filecontents['scrape-detail'] = replaceContent
+
+    uploadByteStream = bytes(json.dumps(filecontents).encode('UTF-8'))
+
     return {
         'statusCode': 200,
         'body': json.dumps('Item Deleted successfully and Content file has been updated')
