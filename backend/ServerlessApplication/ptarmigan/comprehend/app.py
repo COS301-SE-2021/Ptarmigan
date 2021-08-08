843d84a3b4ca01b9c1f25f04940761a30e3f0701
Reinier <u18136835@tuks.co.za>
2021-08-08 11:07:12 +0200

    Added batch processing

--- backend/ServerlessApplication/ptarmigan/comprehend/app.py
+++ backend/ServerlessApplication/ptarmigan/comprehend/app.py
@@ -55,7 +55,7 @@ def getSentimentBatch(batchArray, lang):
     for i in response["ResultList"]:
         batchArray[i["Index"]]["Sentiment"] = i["Sentiment"]
         # print("DOne ", batchArray[i["Index"]])
-
+    # Remember to remove sentimens with errors
     return batchArray
 
 
@@ -88,42 +88,6 @@ def lambda_twitterComprehend(event, context):
 
     print("PutItems ", putItem)
 
-
-    # dbClient = boto3.resource("dynamodb")
-    #
-    # tableName = os.environ["TABLE_NAME"]
-    #
-    # table = dbClient.Table(tableName)
-    #
-    # print("TableName",tableName)
-    #
-    # comprehend = boto3.client("comprehend")
-    #
-    # print(event)
-    #
-    # putItem = []
-    #
-    # for item in event:
-    #     response = comprehend.detect_sentiment(Text=event[item]["Text"], LanguageCode=event[item]["lang"])
-    #     event[item]["sentiment"] = response["Sentiment"]
-    #
-    #     newItem = {
-    #         'PutRequest': {
-    #             'Item': {
-    #                 'Tweet_Id': int(event[item]["Tweet Id"]),
-    #                 'Text': event[item]["Text"],
-    #                 'lang': event[item]["lang"],
-    #                 'Weight': str(event[item]["Weight"]),
-    #                 'Sentiment': event[item]["sentiment"],
-    #                 'TimeStamp': event[item]["date"],
-    #                 'CompanyName': event[item]["Company"]
-    #             }
-    #         }
-    #     }
-    #     putItem.append(newItem)
-    #
-    # print("plasing items", json.dumps(putItem))
-    #
     dbClient.batch_write_item(
         RequestItems = {
             tableName : putItem
