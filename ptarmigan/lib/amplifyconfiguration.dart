b502e4a9d9a325b44ef4899952100d81d84945b4
L_Bradford <u18185208@tuks.co.za>
2021-08-11 21:27:07 +0200

    Update amplifyconfiguration.dart

--- ptarmigan/lib/amplifyconfiguration.dart
+++ ptarmigan/lib/amplifyconfiguration.dart
@@ -1,5 +1,5 @@
 const amplifyconfig = ''' {
-    "UserAgen": "aws-amplify-cli/2.0",
+    "UserAgent": "aws-amplify-cli/2.0",
     "Version": "1.0",
     "api": {
         "plugins": {
@@ -22,7 +22,29 @@ const amplifyconfig = ''' {
                 "IdentityManager": {
                     "Default": {}
                 },
-
+                "AppSync": {
+                    "Default": {
+                        "ApiUrl": "https://6mdpm2tiijbx5jxsfjcekazodm.appsync-api.eu-west-1.amazonaws.com/graphql",
+                        "Region": "eu-west-1",
+                        "AuthMode": "API_KEY",
+                        "ApiKey": "da2-j52s3vopxzaknljn2dhnqquoqm",
+                        "ClientDatabasePrefix": "Ptarmigan_API_KEY"
+                    },
+                    "Ptarmigan_AWS_IAM": {
+                        "ApiUrl": "https://6mdpm2tiijbx5jxsfjcekazodm.appsync-api.eu-west-1.amazonaws.com/graphql",
+                        "Region": "eu-west-1",
+                        "AuthMode": "AWS_IAM",
+                        "ClientDatabasePrefix": "Ptarmigan_AWS_IAM"
+                    }
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "eu-west-1:5c966d13-0216-4546-b8cf-df082965aced",
+                            "Region": "eu-west-1"
+                        }
+                    }
+                },
                 "CognitoUserPool": {
                     "Default": {
                         "PoolId": "eu-west-1_gM8mCo99w",
@@ -32,7 +54,24 @@ const amplifyconfig = ''' {
                 },
                 "Auth": {
                     "Default": {
-                        "authenticationFlowType": "USER_SRP_AUTH"
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "loginMechanism": [],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": [
+                                "REQUIRES_LOWERCASE",
+                                "REQUIRES_NUMBERS",
+                                "REQUIRES_SYMBOLS",
+                                "REQUIRES_UPPERCASE"
+                            ]
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ]
                     }
                 }
             }
