dfa1ebe1867c4c3858cf75c903985df93a3d9d99
L_Bradford <u18185208@tuks.co.za>
2021-08-12 22:52:19 +0200

    Web embed implemented
    Can view twitter page in app.

--- ptarmigan/lib/widgets/home_page.dart
+++ ptarmigan/lib/widgets/home_page.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
 import 'dart:math';
 
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:social_embed_webview/platforms/twitter.dart';
+import 'package:social_embed_webview/social_embed_webview.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class DashboardScreen extends StatefulWidget {
   @override
@@ -15,14 +19,15 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   void initState() {
     print("Home page loaded.");
+    //if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
     super.initState();
-   // Amplify.Auth.getCurrentUser().then((user) {
+    // Amplify.Auth.getCurrentUser().then((user) {
     //  setState(() {
-     //   _user = user;
-     // });
-   // }).catchError((error) {
-   //   print(error.message as AuthException);
-  //  });
+    //   _user = user;
+    // });
+    // }).catchError((error) {
+    //   print(error.message as AuthException);
+    //  });
   }
 
   @override
@@ -33,52 +38,28 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-        actions: [
-          MaterialButton(
-              onPressed: () {
-                Amplify.Auth.signOut().then((_) {
-                  Navigator.pushReplacementNamed(context, '/login');
-                });
-              },
-              child: Icon(
-                Icons.logout,
-                color: Colors.white,
-              ))
-        ],
-      ),
-      //Drawer
-      drawer: Drawer(),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Container(
-            width: double.infinity,
-            height: 100,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(
-                color: Color(0xFFE6E6E6),
-              )
-            ),
-            child: Padding(padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-              child: Row(mainAxisAlignment: 
-                MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Text(
-                    '<Tweet 1>'
-                  )
-                  SocialEmbed()
-                ],
-
-            ),
-            ),
-          )
-        ],
-        ]
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Dashboard'),
+          actions: [
+            MaterialButton(
+                onPressed: () {
+                  Amplify.Auth.signOut().then((_) {
+                    Navigator.pushReplacementNamed(context, '/login');
+                  });
+                },
+                child: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+        //Drawer
+        drawer: Drawer(),
+        body: WebView(
+          initialUrl:
+              'https://twitter.com/ZacksJerryRig/status/1424895584075354118',
+          //'https://www.youtube.com/watch?v=HNODkS9gZmM',
+          javascriptMode: JavascriptMode.unrestricted,
+        ));
   }
 }
