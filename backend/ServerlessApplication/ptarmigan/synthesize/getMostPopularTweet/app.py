94c967350a6396d8290a0b959e27304748975c80
Reinier <u18136835@tuks.co.za>
2021-08-14 12:02:11 +0200

    added processing to return the highest

--- backend/ServerlessApplication/ptarmigan/synthesize/getMostPopularTweet/app.py
+++ backend/ServerlessApplication/ptarmigan/synthesize/getMostPopularTweet/app.py
@@ -10,10 +10,6 @@ def getMostPopularTweet(companyName, beginDate, endDate):
     dynamodb = boto3.resource('dynamodb')
     tableName = companyName
     table = dynamodb.Table(tableName)
-    high = {
-        "TweetId" : 0,
-        "Weight" : 0
-    }
 
     # Getting all of the tweets in a given timeframe from the DB
 
@@ -24,13 +20,11 @@ def getMostPopularTweet(companyName, beginDate, endDate):
 
     # Loop through the results and setting the highest one to high
 
+    high = response["Items"][0]
+
     for i in response["Items"]:
         if int(i["Weight"]) > int(high["Weight"]):
             high = i
-
-
-
-
 
     print(response)
 
@@ -39,13 +33,29 @@ def getMostPopularTweet(companyName, beginDate, endDate):
 
 def lambda_handler(event, context):
     print("Starting")
-    if "body" in event:
-        data = json.loads(event["body"])
+    try:
+        if "body" in event:
+            data = json.loads(event["body"])
 
-    else:
-        data = event
+        else:
+            data = event
 
+        returnData = getMostPopularTweet(data["CompanyName"], data["BeginDate"], data["EndDate"])
+
+        ret = {
+            "Tweet_Id": str(returnData["Tweet_Id"]),
+            "Weight": str(returnData["Weight"])
+        }
         return {
             "statusCode": 200,
-            "body": getMostPopularTweet(data["CompanyName"], data["BeginDate"], data["EndDate"]),
+            "body": json.dumps(ret)
         }
+
+    except:
+        return {
+            "statusCode": 400,
+            "body": json.dumps("Invalid Inputs")
+        }
+
+
+
