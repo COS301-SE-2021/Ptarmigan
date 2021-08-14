822f85c6624f2472161d1f8a789e6037976b33a7
L_Bradford <u18185208@tuks.co.za>
2021-08-14 21:56:52 +0200

    Displays tweet page with given id

--- ptarmigan/lib/widgets/twitter_page.dart
+++ ptarmigan/lib/widgets/twitter_page.dart
@@ -4,32 +4,36 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class TwitterScreen extends StatelessWidget {
-  const TwitterScreen({ Key? key }) : super(key: key);
+  // ignore: non_constant_identifier_names
+  late String twitter_url;
+  TwitterScreen(url) {
+    twitter_url = "https://twitter.com/user/status/";
+    twitter_url += url;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text('Dashboard'),
-          actions: [
-            MaterialButton(
-                onPressed: () {
-                  Amplify.Auth.signOut().then((_) {
-                    Navigator.pushReplacementNamed(context, '/login');
-                  });
-                },
-                child: Icon(
-                  Icons.logout,
-                  color: Colors.white,
-                ))
-          ],
-        ),
-        body: WebView(
-  initialUrl:
-  'https://twitter.com/ZacksJerryRig/status/1424895584075354118',
-  //'https://www.youtube.com/watch?v=HNODkS9gZmM',
-  javascriptMode: JavascriptMode.unrestricted,
-  ),
+        title: Text('Dashboard'),
+        actions: [
+          MaterialButton(
+              onPressed: () {
+                Amplify.Auth.signOut().then((_) {
+                  Navigator.pushReplacementNamed(context, '/login');
+                });
+              },
+              child: Icon(
+                Icons.logout,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: WebView(
+        initialUrl: twitter_url,
+        //'https://www.youtube.com/watch?v=HNODkS9gZmM',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
     );
   }
 }
