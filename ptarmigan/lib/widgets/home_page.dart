4352b5afba31fb425d3f2e24da0dc42e0e43b6b1
L_Bradford <u18185208@tuks.co.za>
2021-08-14 22:41:23 +0200

    Update home_page.dart

--- ptarmigan/lib/widgets/home_page.dart
+++ ptarmigan/lib/widgets/home_page.dart
@@ -80,30 +80,64 @@ class _DashboardScreenState extends State<DashboardScreen> {
               ],
             ),
             CarouselSlider(
-              options: CarouselOptions(height: 400.0),
-              items: [
-                "https://logo.clearbit.com/tesla.com",
-                "https://logo.clearbit.com/bitcoin.com",
-                "https://logo.clearbit.com/neuralink.com",
-                "https://logo.clearbit.com/apple.com",
-                "https://logo.clearbit.com/microsoft.com"
-              ]
-                  .map((item) => Container(
-                          child: Center(
-                        child: GestureDetector(
-                            onTap: () {
-                              print("Image : " +
-                                  item +
-                                  "  ==== HAS BEEN CLICKED");
-                            },
-                            child: Image.network(item,
-                                fit: BoxFit.cover, width: 1000)),
-                      )))
-                  .toList(),
+              options: CarouselOptions(
+                autoPlay: true,
+                aspectRatio: 2.0,
+                enlargeCenterPage: true,
+              ),
+              items: imageSliders,
             )
           ]),
         ));
   }
+
+  final List<Widget> imageSliders = [
+    "https://logo.clearbit.com/tesla.com",
+    "https://logo.clearbit.com/bitcoin.com",
+    "https://logo.clearbit.com/neuralink.com",
+    "https://logo.clearbit.com/apple.com",
+    "https://logo.clearbit.com/microsoft.com"
+  ]
+      .map((item) => Container(
+            child: Container(
+              margin: EdgeInsets.all(5.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                  child: Stack(
+                    children: <Widget>[
+                      Image.network(item, fit: BoxFit.cover, width: 1000.0),
+                      Positioned(
+                        bottom: 0.0,
+                        left: 0.0,
+                        right: 0.0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromARGB(200, 0, 0, 0),
+                                Color.fromARGB(0, 0, 0, 0)
+                              ],
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                            ),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 20.0),
+                          child: Text(
+                            "image demo",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+            ),
+          ))
+      .toList();
 }
 
 /*
