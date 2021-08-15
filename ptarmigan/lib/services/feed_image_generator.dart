e62345530ea0c18fc755c011bee299ac1ecc73fd
L_Bradford <u18185208@tuks.co.za>
2021-08-15 19:23:28 +0200

    Update feed_image_generator.dart

--- ptarmigan/lib/services/feed_image_generator.dart
+++ ptarmigan/lib/services/feed_image_generator.dart
@@ -12,30 +12,45 @@ class FeedImage {
 
   FeedImage({required this.until, required this.contents});
 
-  factory FeedImage.fromJson(Map<String, dynamic> json) {
-    return FeedImage(
-        until: json['Scrape-until'], contents: json['scrape-detail']);
-  }
-
-  void printContents() {
-    print(contents);
-  }
+  FeedImage.fromJson(Map<String, dynamic> json)
+      : until = json['Scrape-until'],
+        contents = json['scrape-detail'];
 }
 
 class FeedImageGenerator {
-  FeedImageGenerator();
+  FeedImageGenerator({this.contents});
+  final contents;
 
   Future<http.Response> fetchFeeds() {
     return http.post(Uri.parse(
         "https://cn9x0zd937.execute-api.eu-west-1.amazonaws.com/Prod/scraper/returnBucketList"));
   }
 
-  Future<FeedImage> fetchImages() async {
+  Future<List> fetchImages() async {
+    //print("calling api for feed list ");
     final response = await http.post(Uri.parse(
         "https://cn9x0zd937.execute-api.eu-west-1.amazonaws.com/Prod/scraper/returnBucketList"));
 
+    //print(response.body);
+    var res = FeedImage.fromJson(jsonDecode(response.body));
     //if (response.statusCode == 200) {
-    return FeedImage.fromJson(jsonDecode(response.body));
+    //print(res.contents[1]);
+
+    var temp = jsonEncode(res.contents);
+    //print("temp : " + temp);
+
+    var finalContents = jsonDecode(temp);
+    //print(finalContents[1]);
+
+    for (var i = 0; i < finalContents.length; i++) {
+      String hold = finalContents[i].toString();
+      hold = hold.substring(10, hold.length - 1);
+      hold = hold.trim();
+      finalContents[i] = hold;
+    }
+    //print(finalContents);
+
+    return finalContents;
     // } else {
     //  throw Exception("Failed to fetch images");
     //}
