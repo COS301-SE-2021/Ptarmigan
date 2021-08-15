4eb2c628f4f4dcd7fad53dae24623208fca5a660
TylerArro <u18033882@tuks.co.za>
2021-08-15 14:03:23 +0200

    updated getting user pool function for admin frontend

---
+++ AdminFrontEnd/index.js
@@ -0,0 +1,27 @@
+
+
+$(document).ready(function () {
+    //initalize credentials
+    AWS.config.region = 'eu-west-1'; // Region
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'eu-west-1:16273994-4cdf-42fd-b2f9-48c1728f6902',
+    });
+    //initalize cognito service
+    var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'});
+
+    var params = {
+        UserPoolId: 'eu-west-1_nn8eU3DXM', /* required */
+        AttributesToGet: [],
+        Filter: '',
+        Limit: '100',
+        PaginationToken: ''
+        };
+    cognitoidentityserviceprovider.listUsers(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else
+            {
+                userListData = data
+                console.log(data);
+            }           // successful response
+    });
+
+});
