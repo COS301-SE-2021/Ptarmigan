0f5edf83bb780a6f9bbbe337a866bb0af70f93f9
L_Bradford <u18185208@tuks.co.za>
2021-08-16 22:55:11 +0200

    Created this service to assist with Stock api calls

---
+++ ptarmigan/lib/services/stock_price_generator.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:ptarmigan/models/Feed.dart';
+
+class StockPrices {
+  final double code;
+  final List<dynamic> body;
+
+  StockPrices({required this.code, required this.body});
+
+  StockPrices.fromJson(Map<String, dynamic> json)
+      : code = json['statusCode'],
+        body = json['body'];
+}
+
+class StockPriceGenerator {
+  final contents;
+  StockPriceGenerator({this.contents});
+
+  Future<Map> fetchPrices() async {
+    print("Attempting to access Stock API");
+    //print("calling api for feed list ");
+    final response = await http.post(Uri.parse(
+        "https://cn9x0zd937.execute-api.eu-west-1.amazonaws.com/Prod/stocks/getStockPrice"));
+
+    print(response.body);
+    var code = response.statusCode;
+    print(code);
+    Map<String, dynamic> res = jsonDecode(response.body);
+    //if (response.statusCode == 200) {
+    print(res['Bitcoin']);
+
+    //print("temp : " + temp);
+
+    //print(finalContents[1]);
+
+    /* for (var i = 0; i < finalContents.length; i++) {
+      String hold = finalContents[i].toString();
+      hold = hold.substring(10, hold.length - 1);
+      hold = hold.trim();
+      finalContents[i] = hold;
+    }*/
+    //print(finalContents);
+
+    return res;
+    // } else {
+    //  throw Exception("Failed to fetch images");
+    //}
+  }
+}
