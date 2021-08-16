dd991cd223d966179caae45bcbeb2ac6265a0744
TylerArro <u18033882@tuks.co.za>
2021-08-16 11:17:21 +0200

    Added commends and cleaned up code

--- backend/ServerlessApplication/ptarmigan/Stocks/getStockPrice/app.py
+++ backend/ServerlessApplication/ptarmigan/Stocks/getStockPrice/app.py
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
     }
 
 def getList():
-
+    #get list from s3 Bucket
     s3client = boto3.client('s3')
 
     bucketname = 'stepfunctestbucket'
@@ -48,19 +48,22 @@ def getList():
 
 def getPriceList(list):
     listPrices = []
-
+    # Make api request to get price based on ticker symbols
     for stuff in list:
         requestUrl = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stuff}&apikey=VDLMI3ZNV3LSSLDZ"
         requestReturn = requests.get(requestUrl)
         requestResults = json.loads(requestReturn.text)
-
+        # Make request to crypo api if error returns from stock api
         if ([*requestResults.keys()])[0] == "Error Message":
+            #pull crypto symblo from ticker
             crypto = stuff[2:5]
+            # add appropriate field to list
             requestUrlCrypto = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={crypto}&to_currency=USD&apikey=VDLMI3ZNV3LSSLDZ"
             requestReturnCrypto = requests.get(requestUrlCrypto)
             requestResultsCrypto = json.loads(requestReturnCrypto.text)
             listPrices.append((requestResultsCrypto['Realtime Currency Exchange Rate'])['5. Exchange Rate'])
         else:
+            #add appropriate field to list
             requestResults = requestResults['Time Series (Daily)']
             key1 = ([*requestResults.keys()])[0]
             listPrices.append((requestResults[key1])['4. close'])
@@ -70,12 +73,14 @@ def getPriceList(list):
 def getTickerSymbols():
     tickerSymbolList = []
     # should probably change this functions api since its pepega
+    # Make api request to get ticker symbol based on search query
     for stuff in list:
+        #api requerst to sock api to get ticker
         requestUrl = f"https://api.polygon.io/v3/reference/tickers?market=stocks&search={stuff} &active=true&sort=ticker&order=asc&limit=10&apiKey=PNqoXU3luX7smsggLGPacHd8JnKZkDMV"
         requestReturn = requests.get(requestUrl)
         requestReturn = json.loads(requestReturn.text)
         requestResults = requestReturn['results']
-
+        # Make request to crypo api if error returns from stock api
         if requestResults == None:
 
             requestUrlCrypto = f"https://api.polygon.io/v3/reference/tickers?market=crypto&search={stuff} Dollar&order=asc&limit=10&apiKey=PNqoXU3luX7smsggLGPacHd8JnKZkDMV"
@@ -84,5 +89,6 @@ def getTickerSymbols():
             requestResultsCrypto = requestReturnCrypto['results']
             tickerSymbolList.append(requestResultsCrypto[0]['ticker'])
         else:
+            # add appropriate field to list
             tickerSymbolList.append(requestResults[0]['ticker'])
     return (tickerSymbolList)
