a07415389e005e1800b78e1096dc4778b126b550
TylerArro <u18033882@tuks.co.za>
2021-08-16 23:50:05 +0200

    create returnPriceList function

--- backend/ServerlessApplication/ptarmigan/stocks/returnStockPriceList/app.py
+++ backend/ServerlessApplication/ptarmigan/stocks/returnStockPriceList/app.py
@@ -1,8 +1,29 @@
 import json
 import boto3
 
+
 def lambda_handler(event, context):
-    return {
-        'statusCode': 404,
-        'body': json.dumps('Bucket/item not found')
-    }
+    return returnListFromS3()
+
+
+def returnListFromS3():
+    try:
+        s3client = boto3.client('s3')
+        bucketname = 'stepfunctestbucket'
+        file_to_read = 'priceList.json'
+        fileobj = s3client.get_object(
+            Bucket=bucketname,
+            Key=file_to_read
+        )
+
+        filedata = fileobj['Body'].read()
+        filecontents = (filedata.decode('utf-8'))
+        return {
+            'statusCode': 200,
+            'body': json.loads(filecontents)
+        }
+    except:
+        return {
+            'statusCode': 404,
+            'body': json.dumps('Bucket/item not found')
+        }
