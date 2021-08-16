ea6cfae534d64c2d96bbc69f066f4cfb4cea559b
L_Bradford <u18185208@tuks.co.za>
2021-08-16 08:29:03 +0200

    Created file to store all colours and settings used throughout the app

---
+++ ptarmigan/lib/settings/application_settings.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Settings {
+  static const backgroundColor = const Color(0xff232d37);
+  final barColor = 0xff07424B;
+  final primaryTextColor = Colors.white;
+  final secondaryTextColor = Colors.black26;
+
+  Color getBackgroundColor() {
+    return backgroundColor;
+  }
+}
