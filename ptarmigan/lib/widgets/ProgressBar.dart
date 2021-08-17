d0a320a18278564d7048ea0ad2c10e7572888633
u17095230 <u17095230@tuks.co.za>
2021-08-17 16:07:24 +0200

    Progress Bar Added
    Class for the use of Progress Bar added

---
+++ ptarmigan/lib/widgets/ProgressBar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class ProgressBar extends StatelessWidget {
+  const ProgressBar({
+    Key? key,
+    this.color = primaryColor,
+    required this.percentage,
+  }) : super(key: key);
+
+  final Color? color;
+  final int? percentage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 5,
+          decoration: BoxDecoration(
+            color: color!.withOpacity(0.1),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+        ),
+        LayoutBuilder(
+          builder: (context, constraints) => Container(
+            width: constraints.maxWidth * (percentage! / 100),
+            height: 5,
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
