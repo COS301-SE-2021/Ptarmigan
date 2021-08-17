65d1bc0fee1194639d9075d39c866a907ef6763f
u17095230 <u17095230@tuks.co.za>
2021-08-17 17:01:43 +0200

    Object to store sentiment values
    Object can be used to store sentiment values in the UI

---
+++ ptarmigan/lib/widgets/SentimentHistoryItem.dart
@@ -0,0 +1,134 @@
+class SentimentHistoryItem {
+  final String? icon, title, date, size;
+
+  SentimentHistoryItem({this.icon, this.title, this.date, this.size});
+}
+
+List demoRecentFiles = [
+  SentimentHistoryItem(
+    icon: "assets/icons/Medium.svg",
+    title: "01-03-2021",
+    date: "62%",
+    size: "3.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "19.0mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "32.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "3.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "2.5gb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/pdf_file.svg",
+    title: "01-03-2021",
+    date: "99%",
+    size: "3.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "34.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Medium.svg",
+    title: "01-03-2021",
+    date: "62%",
+    size: "3.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "19.0mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "32.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "3.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "2.5gb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Positive.svg",
+    title: "01-03-2021",
+    date: "99%",
+    size: "3.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "34.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Medium.svg",
+    title: "01-03-2021",
+    date: "62%",
+    size: "3.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "19.0mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "32.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "3.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "2.5gb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Positive.svg",
+    title: "01-03-2021",
+    date: "99%",
+    size: "3.5mb",
+  ),
+  SentimentHistoryItem(
+    icon: "assets/icons/Negative.svg",
+    title: "01-03-2021",
+    date: "30%",
+    size: "34.5mb",
+  ),
+];
