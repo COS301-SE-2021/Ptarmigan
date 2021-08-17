698143896ce6ffc2915a3e961806cc7d8a19b249
u17095230 <u17095230@tuks.co.za>
2021-08-17 17:53:05 +0200

    Stock and Sentiment History Widgets added
    Added Stock and Sentiment History widgets have been added to show prior values

--- ptarmigan/lib/widgets/header.dart
+++ ptarmigan/lib/widgets/header.dart
@@ -1,7 +1,5 @@
-import 'package:admin/controllers/MenuController.dart';
-import 'package:admin/responsive.dart';
+import '../controllers/MenuController.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 import '../../../constants.dart';
@@ -15,18 +13,10 @@ class Header extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        if (!Responsive.isDesktop(context))
-          IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: context.read<MenuController>().controlMenu,
-          ),
-        if (!Responsive.isMobile(context))
-          Text(
-            "Dashboard",
-            style: Theme.of(context).textTheme.headline6,
-          ),
-        if (!Responsive.isMobile(context))
-          Spacer(flex: Responsive.isDesktop(context) ? 2 : 1),
+        IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: context.read<MenuController>().controlMenu,
+        ),
       ],
     );
   }
---
+++ ptarmigan/lib/widgets/stockHistory.dart
@@ -0,0 +1,79 @@
+import '/models/SentimentHistoryItem.dart';
+import 'package:data_table_2/data_table_2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../constants.dart';
+
+class StockHistory extends StatelessWidget {
+  const StockHistory({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(defaultPadding),
+      decoration: BoxDecoration(
+        color: secondaryColor,
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Stock History",
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+          SizedBox(
+            height: 400,
+            width: double.infinity,
+            child: DataTable2(
+              columnSpacing: defaultPadding,
+              minWidth: 600,
+              columns: [
+                DataColumn(
+                  label: Text("Date"),
+                ),
+                DataColumn(
+                  label: Text("Price"),
+                ),
+                DataColumn(
+                  label: Text("Size"),
+                ),
+              ],
+              rows: List.generate(
+                demoRecentFiles.length,
+                (index) => recentFileDataRow(demoRecentFiles[index]),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+DataRow recentFileDataRow(SentimentHistoryItem fileInfo) {
+  return DataRow(
+    cells: [
+      DataCell(
+        Row(
+          children: [
+            SvgPicture.asset(
+              fileInfo.icon!,
+              height: 30,
+              width: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+              child: Text(fileInfo.title!),
+            ),
+          ],
+        ),
+      ),
+      DataCell(Text(fileInfo.date!)),
+      DataCell(Text(fileInfo.size!)),
+    ],
+  );
+}
