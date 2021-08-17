8163d21d56aba8d71829f23b763c3065583a0468
Benjamin <u04882530@tuks.co.za>
2021-08-17 14:17:35 +0200

    update

--- AdminFrontEnd/login.js
+++ AdminFrontEnd/login.js
@@ -13,28 +13,16 @@ $(document).ready(function () {
         //PaginationToken: '1'
     };
 
-    cognitoidentityserviceprovider.listUsers(params, function(err, data) {
+    var paramsAdminUsers = {
+        GroupName: 'Admin', /* required */
+        UserPoolId: 'eu-west-1_gM8mCo99w', /* required */
+        Limit: '50',
+    };
+    cognitoidentityserviceprovider.listUsersInGroup(paramsAdminUsers, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else
-        {
-            // successful response
-            var adminData
-            var userListData = data
-            length = userListData["Users"]["length"]
-            setUsers(length)
-
-            var paramsAdminUsers = {
-                GroupName: 'Admin', /* required */
-                UserPoolId: 'eu-west-1_gM8mCo99w', /* required */
-                Limit: '50',
-            };
-            cognitoidentityserviceprovider.listUsersInGroup(paramsAdminUsers, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                else
-                    adminData = data;
-                //console.log("Users in admin group returned");           // successful response
-                userTable(userListData["Users"],adminData["Users"])
-            });
-        }
+            adminData = data;
+        console.log("Users in admin group returned");           // successful response
+        userTable(userListData["Users"],adminData["Users"])
     });
 });
