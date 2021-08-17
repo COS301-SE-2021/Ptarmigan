26d4216a388906bdb2e2a5f68fec5ccf512aa6e6
TylerArro <u18033882@tuks.co.za>
2021-08-17 23:46:57 +0200

    unit testing for twitterscraper function

--- backend/ServerlessApplication/tests/unit/test_twitterScraper.py
+++ backend/ServerlessApplication/tests/unit/test_twitterScraper.py
@@ -9,18 +9,20 @@ from ptarmigan.scraper.twitterScraper import app
 class TestClass(TestCase):
 
     def setUp(self):
-        self.test_tweet_context = '{ "Scrape-until": "123421343214",' \
+        self.test_tweet_context = '{ "Scrape-until": "1629235961",' \
                                     '"content": {' \
                                     '"content": "IBM"}}'\
 
-    @patch('backend.sam.twitterScraper.lambda_scrapet.returnTweetList')
+
+    @patch('ptarmigan.scraper.twitterScraper.app.return_tweet_list')
+
     def test_IfScrapetReturnsTweetsWithCorrectData(self, mock_scrape):
         expected = ({
             "0": {
                 "Tweet Id": 100000000001,
                 "Text": "A",
                 "lang": "en",
-                "date": 1622746104001,
+                "date": 1629235961,
                 "Weight": 21,
                 "Company": "IBM"
             },
@@ -28,7 +30,7 @@ class TestClass(TestCase):
                 "Tweet Id": 100000000002,
                 "Text": "E",
                 "lang": "en",
-                "date": 1622746104002,
+                "date": 1629235961,
                 "Weight": 25,
                 "Company": "IBM"
             },
@@ -36,7 +38,7 @@ class TestClass(TestCase):
                 "Tweet Id": 100000000003,
                 "Text": "I",
                 "lang": "en",
-                "date": 1622746104003,
+                "date": 1629235961,
                 "Weight": 29,
                 "Company": "IBM"
             },
@@ -44,7 +46,7 @@ class TestClass(TestCase):
                 "Tweet Id": 100000000004,
                 "Text": "O",
                 "lang": "en",
-                "date": 1622746104004,
+                "date": 1629235961,
                 "Weight": 33,
                 "Company": "IBM"
             },
@@ -52,19 +54,17 @@ class TestClass(TestCase):
                 "Tweet Id": 100000000005,
                 "Text": "U",
                 "lang": "en",
-                "date": 1622746104005,
+                "date": 1629235961,
                 "Weight": 37,
                 "Company": "IBM"
             }
         })
-
-        expected = json.dumps(expected, indent=4)
         # mock data for scraper library
-        mock_scrape.return_value = [[100000000001, "A", 5, 5, "en", 1622746104001],
-                                    [100000000002, "E", 6, 6, "en", 1622746104002],
-                                    [100000000003, "I", 7, 7, "en", 1622746104003],
-                                    [100000000004, "O", 8, 8, "en", 1622746104004],
-                                    [100000000005, "U", 9, 9, "en", 1622746104005]]
+        mock_scrape.return_value = [[100000000001, "A", 5, 5, "en", 1629235961],
+                                    [100000000002, "E", 6, 6, "en", 1629235961],
+                                    [100000000003, "I", 7, 7, "en", 1629235961],
+                                    [100000000004, "O", 8, 8, "en", 1629235961],
+                                    [100000000005, "U", 9, 9, "en", 1629235961]]
         # call function
-        testReturn = (app.scraperHandler(json.loads(self.test_tweet_context), ""))
+        testReturn = (app.scraper_handler(json.loads(self.test_tweet_context), ""))
         assert testReturn == expected
