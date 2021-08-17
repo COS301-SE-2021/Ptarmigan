18e4d6e9d0186f2122ee5cd0e48c130d48bd98d2
u17095230 <u17095230@tuks.co.za>
2021-08-17 17:25:09 +0200

    Added dashboard header
    Header for the dash board added

--- ptarmigan/lib/widgets/graph.dart
+++ ptarmigan/lib/widgets/graph.dart
@@ -1,27 +1,18 @@
 // @dart=2.9
 
 import 'dart:async';
+import '../../../constants.dart';
+
 // flutter and ui libraries
-import 'package:amplify_auth_cognito/method_channel_auth_cognito.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // amplify packages we will need to use
-import 'package:amplify_flutter/amplify.dart';
-import 'package:amplify_datastore/amplify_datastore.dart';
 import 'package:provider/provider.dart';
-import 'package:ptarmigan/models/ModelProvider.dart';
-import 'package:ptarmigan/widgets/feeds_list_admin.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 // amplify configuration and models that should have been generated for you
-import 'package:amplify_api/amplify_api.dart';
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:bezier_chart/bezier_chart.dart';
 //for feeds go to feeds_list.dart
 
 class Graph extends StatefulWidget {
-  List<Feed> feeds;
-  Graph({this.feeds});
-
   @override
   _GraphState createState() => _GraphState();
 }
@@ -34,6 +25,86 @@ class _GraphState extends State<Graph> {
     final date1 = DateTime.now().subtract(Duration(days: 2));
     final date2 = DateTime.now().subtract(Duration(days: 3));
 
+    return Container(
+      //Grapg Container
+      height: 240,
+      decoration: BoxDecoration(
+        color: secondaryColor,
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
+      padding: EdgeInsets.only(
+        left: 0.0,
+        right: 0.0,
+        top: 40,
+        bottom: 0,
+      ),
+      child: Container(
+        width: 800,
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(18),
+            ),
+            color: Colors.black45),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(right: 0.0, left: 0.0, top: 0, bottom: 20),
+          child: BezierChart(
+            fromDate: fromDate,
+            bezierChartScale: BezierChartScale.WEEKLY,
+            toDate: toDate,
+            selectedDate: toDate,
+            series: [
+              BezierLine(
+                lineColor: Colors.green,
+                lineStrokeWidth: 2.0,
+                //   label: "Dutysdas",
+                onMissingValue: (dateTime) {
+                  if (dateTime.day.isEven) {
+                    return 0.0;
+                  }
+                  return 0.0;
+                },
+                data: //list, //graphPoints
+                    [
+                  DataPoint<DateTime>(
+                      value: 10,
+                      xAxis: DateTime.now().subtract(Duration(days: 2))),
+                  DataPoint<DateTime>(
+                      value: 130,
+                      xAxis: DateTime.now().subtract(Duration(days: 3))),
+                  DataPoint<DateTime>(
+                      value: 50,
+                      xAxis: DateTime.now().subtract(Duration(days: 4))),
+                  DataPoint<DateTime>(
+                      value: 150,
+                      xAxis: DateTime.now().subtract(Duration(days: 2))),
+                  DataPoint<DateTime>(
+                      value: 75,
+                      xAxis: DateTime.now().subtract(Duration(days: 2))),
+                  DataPoint<DateTime>(
+                      value: 0,
+                      xAxis: DateTime.now().subtract(Duration(days: 2))),
+                  DataPoint<DateTime>(
+                      value: 5,
+                      xAxis: DateTime.now().subtract(Duration(days: 2))),
+                  DataPoint<DateTime>(
+                      value: 45,
+                      xAxis: DateTime.now().subtract(Duration(days: 2))),
+                ],
+              ),
+            ],
+            config: BezierChartConfig(
+              verticalIndicatorStrokeWidth: 3.0,
+              verticalIndicatorColor: Colors.black26,
+              showVerticalIndicator: true,
+              verticalIndicatorFixedPosition: false,
+              footerHeight: 50.0,
+            ),
+          ),
+        ),
+      ),
+    );
+/*
     return Center(
         child: AspectRatio(
             aspectRatio: 1.90,
@@ -72,11 +143,12 @@ class _GraphState extends State<Graph> {
                     verticalIndicatorColor: Colors.white,
                     showVerticalIndicator: true,
                     verticalIndicatorFixedPosition: false,
-                    backgroundColor: Colors.amber, // Color(0xff232d37),
                     footerHeight: 30.0,
                   ),
                 ),
               ),
             )));
+  }*/
+//}
   }
 }
---
+++ ptarmigan/lib/widgets/header.dart
@@ -0,0 +1,33 @@
+import 'package:admin/controllers/MenuController.dart';
+import 'package:admin/responsive.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+
+import '../../../constants.dart';
+
+class Header extends StatelessWidget {
+  const Header({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        if (!Responsive.isDesktop(context))
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: context.read<MenuController>().controlMenu,
+          ),
+        if (!Responsive.isMobile(context))
+          Text(
+            "Dashboard",
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        if (!Responsive.isMobile(context))
+          Spacer(flex: Responsive.isDesktop(context) ? 2 : 1),
+      ],
+    );
+  }
+}
