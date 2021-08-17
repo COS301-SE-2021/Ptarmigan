0806c5845bc5af971b0e3901f6b998121f90daec
TylerArro <u18033882@tuks.co.za>
2021-08-17 12:51:17 +0200

    updated getStockPrice import and function call

--- backend/ServerlessApplication/ptarmigan/stocks/getStockPrice/app.py
+++ backend/ServerlessApplication/ptarmigan/stocks/getStockPrice/app.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import boto3
+import time
 
 
 def lambda_handler(event, context):
@@ -12,8 +13,9 @@ def lambda_handler(event, context):
 
     return writeToBucket(priceDict)
 
+
 def getList():
-    #get list from s3 Bucket
+    # get list from s3 Bucket
     s3client = boto3.client('s3')
 
     bucketname = 'stepfunctestbucket'
@@ -49,28 +51,29 @@ def getPriceList(list):
     # Make api request to get price based on ticker symbols
     for stuff in list:
         if apiCalls == 5:
-            sleep(60)
+            time.sleep(60)
             apiCalls = 0
         requestUrl = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stuff}&apikey=VDLMI3ZNV3LSSLDZ"
         requestReturn = requests.get(requestUrl)
-        apiCalls +=1
+        apiCalls += 1
         requestResults = json.loads(requestReturn.text)
         # Make request to crypo api if error returns from stock api
         if not requestResults['Global Quote']:
-            #pull crypto symblo from ticker
+            # pull crypto symblo from ticker
             crypto = stuff[2:5]
             # add appropriate field to list
             requestUrlCrypto = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={crypto}&to_currency=USD&apikey=VDLMI3ZNV3LSSLDZ"
             requestReturnCrypto = requests.get(requestUrlCrypto)
-            apiCalls +=1
+            apiCalls += 1
             requestResultsCrypto = json.loads(requestReturnCrypto.text)
             listPrices.append((requestResultsCrypto['Realtime Currency Exchange Rate'])['5. Exchange Rate'])
         else:
-            #add appropriate field to list
+            # add appropriate field to list
             requestResults = requestResults['Global Quote']
             listPrices.append(requestResults['08. previous close'])
 
     return (listPrices)
+
 
 def getTickerSymbols(list):
     tickerSymbolList = []
@@ -79,9 +82,9 @@ def getTickerSymbols(list):
     # Make api request to get ticker symbol based on search query
     for stuff in list:
         if apiCalls == 5:
-            sleep(60)
+            time.sleep(60)
             apiCalls = 0
-        #api requerst to sock api to get ticker
+        # api requerst to sock api to get ticker
         requestUrl = f"https://api.polygon.io/v3/reference/tickers?market=stocks&search={stuff} &active=true&sort=ticker&order=asc&limit=10&apiKey=PNqoXU3luX7smsggLGPacHd8JnKZkDMV"
         requestReturn = requests.get(requestUrl)
         apiCalls += 1
