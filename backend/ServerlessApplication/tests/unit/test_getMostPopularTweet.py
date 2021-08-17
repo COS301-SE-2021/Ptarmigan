1ce828fdce0f288068703a7827a4d9d8b06bf531
Reinier <u18136835@tuks.co.za>
2021-08-17 23:51:52 +0200

    Added testing for get most popular

--- backend/ServerlessApplication/tests/unit/test_getMostPopularTweet.py
+++ backend/ServerlessApplication/tests/unit/test_getMostPopularTweet.py
@@ -7,11 +7,12 @@ from ptarmigan.synthesize.getMostPopularTweet import app
 class GetMostPopularTweet(unittest.TestCase):
 
     def setUp(self):
-        self.testDatabaseOutput = {"body": {
-            "BeginDate":  1628564655,
-            "EndDate":  1628659505,
-            "CompanyName": "Tesla"
-        }}
+        self.testDatabaseOutput = {
+                "BeginDate":  1628564655,
+                "EndDate":  1628659505,
+                "CompanyName": "Tesla"
+            }
+
 
         # {
         #     "BeginDate": 1628554586,
@@ -20,7 +21,7 @@ class GetMostPopularTweet(unittest.TestCase):
         # }
 
     @patch('ptarmigan.synthesize.getMostPopularTweet.app.dbCall')
-    def test_if_sentiment_calculation_graph_returns_correct_data(self, mock_dbReturn):
+    def test_get_most_popular_tweet(self, mock_dbReturn):
         mock_dbReturn.return_value = {
                     "Items": [
                         {
@@ -31,26 +32,41 @@ class GetMostPopularTweet(unittest.TestCase):
                         'Text': '“gUaRaNtEe yOu CoUld bUy a tEslA” 💀 https://t.co/9RvRZCZODw https://t.co/fynaiadZ1t',
                         'CompanyName': 'Tesla',
                         'lang': 'en'
-                            },
-                        {
-                         'Tweet_Id': 1425311842499502082,
-                         'Sentiment': 'NEUTRAL',
-                         'TimeStamp': 1628655798,
-                         'Weight': '3',
-                         'Text': '@finKlEiNhoRN22 @Irate_Prankster @NorthernAndrew_ The wife reminds me of Grimes married to Tesla',
-                         'CompanyName': 'Tesla',
-                         'lang': 'en'
-                         }
+                            }
                          ]
         }
 
-        expected = {"statusCode": 200,
-             "body":
-              {"Tweet_Id": "1425087591456534529",
-               "Weight": "3108"}
-             }
+        expected = {'statusCode': 200,
+                    'body': '{"Tweet_Id": "1425033022500392962", "Weight": "500"}'
+                    }
 
         output = app.lambda_handler(self.testDatabaseOutput, "")
+        self.assertEquals(output, expected)
+
+    @patch('ptarmigan.synthesize.getMostPopularTweet.app.dbCall')
+    def test_invalid_inputs(self, mock_dbReturn):
+        mock_dbReturn.return_value = {
+            "Items": [
+                {
+                    'Tweet_Id': 1425033022500392962,
+                    'Sentiment': 'NEUTRAL',
+                    'TimeStamp': 1628589323,
+                    'Weight': '500',
+                    'Text': '“gUaRaNtEe yOu CoUld bUy a tEslA” 💀 https://t.co/9RvRZCZODw https://t.co/fynaiadZ1t',
+                    'CompanyName': 'Tesla',
+                    'lang': 'en'
+                }
+            ]
+        }
+
+        invalidInput = {
+            "EndDate": 1628659505,
+            "CompanyName": "Tesla"
+        }
+
+        expected = {'statusCode': 400, 'body': '"Invalid Inputs"'}
+
+        output = app.lambda_handler(invalidInput, "")
         print(output)
         self.assertEquals(output, expected)
 
