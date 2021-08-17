ac41744b6995d75ffcabe467d501dd6f0aa5b264
L_Bradford <u18185208@tuks.co.za>
2021-08-17 22:49:13 +0200

    Completely redid home screen.

--- ptarmigan/lib/widgets/home_page.dart
+++ ptarmigan/lib/widgets/home_page.dart
@@ -6,6 +6,9 @@ import 'package:amplify_flutter/amplify.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ptarmigan/components/home_screen_welcome.dart';
+import 'package:ptarmigan/components/menu_drawer.dart';
+import 'package:ptarmigan/constants.dart';
 import 'package:ptarmigan/services/feed_image_generator.dart';
 import 'package:ptarmigan/settings/application_settings.dart';
 import 'package:ptarmigan/widgets/Settings_page.dart';
@@ -55,18 +58,14 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     Settings settings = Settings();
     return Scaffold(
-        backgroundColor: settings.getBackgroundColor(),
+        backgroundColor: bgColor,
+        drawer: MenuDrawer(),
         appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SettingsScreen()));
-            },
-          ),
+          backgroundColor: bgColor,
+          shadowColor: secondaryColor,
           title: Text('Dashboard'),
           actions: [
-            MaterialButton(
+            /*MaterialButton(
                 onPressed: () {
                   Amplify.Auth.signOut().then((_) {
                     Navigator.pushReplacementNamed(context, '/login');
@@ -75,41 +74,128 @@ class _DashboardScreenState extends State<DashboardScreen> {
                 child: Icon(
                   Icons.logout,
                   color: Colors.white,
-                ))
+                ))*/
           ],
         ),
         //Drawer
 
         body: Container(
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            Container(
-              height: 200,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Expanded(
-                      child: Text(
-                          "View the most influential tweet of the last 24 hours..............................................",
-                          style: TextStyle(fontSize: 20))),
-                  ElevatedButton(
-                      onPressed: () async {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    TwitterScreen("1424895584075354118")));
-                        //TodosPage()));
-                        //await feedimage = generator.fetchImages();
-                        // var feedimage = await generator.fetchImages();
-                        //print("feed image below:");
-                        //print(feedimage);
-                      },
-                      child: Text("tweet"))
-                ],
-              ),
-            ),
-            /*CarouselSlider(
+            width: double.infinity,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child:
+                  Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                WelcomeBox(),
+                SizedBox(
+                  height: 40,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 400,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              alignment: Alignment.center,
+                              height: 150,
+                              width: 150,
+                              decoration: BoxDecoration(
+                                color: secondaryColor,
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: secondaryColor.withOpacity(0.6),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(
+                                        0, 3), // changes position of shadow
+                                  ),
+                                ],
+                              ),
+                              child: Text(
+                                "Curious about our info?",
+                                textAlign: TextAlign.center,
+                              )),
+                          Container(
+                            alignment: Alignment.center,
+                            height: 130,
+                            width: 130,
+                            decoration: BoxDecoration(
+                              color: secondaryColor,
+                              shape: BoxShape.circle,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: secondaryColor.withOpacity(0.6),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(
+                                      0, 3), // changes position of shadow
+                                ),
+                              ],
+                            ),
+                            child: ElevatedButton(
+                                child: Text(
+                                  "Click here for the tweet!",
+                                  textAlign: TextAlign.center,
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => TodosPage()));
+                                },
+                                style: ButtonStyle(
+                                  shape:
+                                      MaterialStateProperty.all(CircleBorder()),
+                                  padding: MaterialStateProperty.all(
+                                      EdgeInsets.all(20)),
+                                  backgroundColor: MaterialStateProperty.all(
+                                      primaryColor), // <-- Button color
+                                  overlayColor:
+                                      MaterialStateProperty.resolveWith<Color?>(
+                                          (states) {
+                                    if (states.contains(MaterialState.pressed))
+                                      return secondaryColor; // <-- Splash color
+                                  }),
+                                )),
+                          )
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                              padding: EdgeInsets.all(20),
+                              alignment: Alignment.center,
+                              height: 190,
+                              width: 200,
+                              decoration: BoxDecoration(
+                                color: secondaryColor,
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: secondaryColor.withOpacity(0.6),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(
+                                        0, 3), // changes position of shadow
+                                  ),
+                                ],
+                              ),
+                              child: Text(
+                                "Have a look at the most impactful tweet of the day!",
+                                textAlign: TextAlign.center,
+                              )),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+
+                /*CarouselSlider(
               options: CarouselOptions(
                 autoPlay: true,
                 aspectRatio: 2.0,
@@ -117,57 +203,92 @@ class _DashboardScreenState extends State<DashboardScreen> {
               ),
               items: imageSliders,
             )*/
-
-            CarouselSlider.builder(
-              itemCount: feedimage.length,
-              options: CarouselOptions(
-                autoPlay: true,
-                aspectRatio: 2.0,
-                enlargeCenterPage: true,
-              ),
-              itemBuilder: (context, index, realIdx) {
-                return Container(
-                    child: Center(
-                  child: Stack(children: [
-                    Image.network(
-                        "https://logo.clearbit.com/" +
-                            feedimage[index] +
-                            ".com",
-                        fit: BoxFit.cover,
-                        width: 1000),
-                    Positioned(
-                      bottom: 0.0,
-                      left: 0.0,
-                      right: 0.0,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromARGB(200, 0, 0, 0),
-                              Color.fromARGB(0, 0, 0, 0)
-                            ],
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                          ),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                            vertical: 10.0, horizontal: 20.0),
-                        child: Text(
-                          feedimage[index],
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
+                SizedBox(height: 40),
+                Text(
+                  "Stocks we provide insights to:",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  color: secondaryColor,
+                  child: CarouselSlider.builder(
+                    itemCount: feedimage.length,
+                    options: CarouselOptions(
+                      autoPlay: true,
+                      aspectRatio: 2.0,
+                      enlargeCenterPage: true,
                     ),
-                  ]),
-                ));
-              },
-            ),
-          ]),
-        ),
+                    itemBuilder: (context, index, realIdx) {
+                      return Container(
+                          child: Center(
+                        child: Stack(children: [
+                          Image.network(
+                              "https://logo.clearbit.com/" +
+                                  feedimage[index] +
+                                  ".com",
+                              fit: BoxFit.cover,
+                              width: 1000),
+                          Positioned(
+                            bottom: 0.0,
+                            left: 0.0,
+                            right: 0.0,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color.fromARGB(200, 0, 0, 0),
+                                    Color.fromARGB(0, 0, 0, 0)
+                                  ],
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter,
+                                ),
+                              ),
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 20.0),
+                              child: Text(
+                                feedimage[index],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ]),
+                      ));
+                    },
+                  ),
+                ),
+                Text(
+                  "About us :",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30),
+                ),
+                Container(
+                  color: secondaryColor,
+                  margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  height: 300,
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Text(
+                        """We are a group of University students who are extremely passionate about learning new technologies and developing cutting edge technology and systems.
+
+                      \n For any enquiries please contact us via our email: 
+                      onemorebytecos301@gmail.com
+
+This code is open source and is available on GitHub: 
+github.com/COS301-SE-2021/Ptarmigan
+                    """,
+                        textAlign: TextAlign.center,
+                      ))
+                    ],
+                  ),
+                )
+              ]),
+            )),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
