c4ed0385bdb6e4cbc7409a3607b89a66611bc668
Benjamin <u04882530@tuks.co.za>
2021-08-17 19:09:42 +0200

    Trying to fix error

--- backend/ServerlessApplication/tests/unit/test_getMostPopularTweet.py
+++ backend/ServerlessApplication/tests/unit/test_getMostPopularTweet.py
@@ -19,6 +19,52 @@ class GetMostPopularTweet(unittest.TestCase):
         #     "CompanyName": "Microsoft"
         # }
 
-    @patch('ptarmigan.synthesize.getGraphSentiment.app.dbReturn')
+    @patch('ptarmigan.synthesize.getGraphSentiment.app.dbCall')
     def test_if_sentiment_calculation_graph_returns_correct_data(self, mock_dbReturn):
-       print("Place your shit over here")
+        mock_dbReturn.return_value = {
+                    "Items": [
+                        {
+                        'Tweet_Id':Decimal('1425033022500392962'),
+                        'Sentiment': 'NEUTRAL',
+                        'TimeStamp': Decimal('1628589323'),
+                        'Weight': '500',
+                        'Text': '“gUaRaNtEe yOu CoUld bUy a tEslA” 💀 https://t.co/9RvRZCZODw https://t.co/fynaiadZ1t',
+                        'CompanyName': 'Tesla',
+                        'lang': 'en'
+                            },
+                        {
+                         'Tweet_Id': Decimal('1425311842499502082'),
+                         'Sentiment': 'NEUTRAL',
+                         'TimeStamp': Decimal('1628655798'),
+                         'Weight': '3',
+                         'Text': '@finKlEiNhoRN22 @Irate_Prankster @NorthernAndrew_ The wife reminds me of Grimes married to Tesla',
+                         'CompanyName': 'Tesla',
+                         'lang': 'en'
+                         },
+                         {
+                         'Tweet_Id': Decimal('1425235864620261376'),
+                         'Sentiment': 'NEUTRAL',
+                         'TimeStamp': Decimal('1628637684'),
+                         'Weight': '2',
+                         'Text': '@LyraKeaton Introducing the Tesla Casket',
+                         'CompanyName': 'Tesla', 'lang': 'en'},
+                         {'Tweet_Id': Decimal('1425294874769862657'),
+                         'Sentiment': 'NEUTRAL',
+                         'TimeStamp': Decimal('1628651753'),
+                         'Weight': '2',
+                         'Text': 'Next door neighbor just brought home a new BMW X5. I told him “nice upgrade!” He asked when I was gonna buy a new car. My reply was simple:\n\n“When Papa @elonmusk gets @Tesla to accept @dogecoin I’ll buy a #ModelY”',
+                         'CompanyName': 'Tesla',
+                          'lang': 'en'
+                          }]
+        }
+
+        expected = {
+            {"statusCode": 200,
+             "body":
+              "{"Tweet_Id": "1425087591456534529",
+               "Weight": "3108"}"}
+        }
+        output = app.lambda_handler(self.testDatabaseOutput, "")
+        print(output)
+        self.assertEquals(200, 200)
+
