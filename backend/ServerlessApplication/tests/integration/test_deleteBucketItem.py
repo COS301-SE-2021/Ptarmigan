1805ebf32628037433e5e01fabf2b4b0cf7baafe
TylerArro <u18033882@tuks.co.za>
2021-08-18 17:28:06 +0200

    Intergration Test for deleteBucketitem

---
+++ backend/ServerlessApplication/tests/integration/test_deleteBucketItem.py
@@ -0,0 +1,27 @@
+import json
+from unittest.mock import patch
+import pytest
+import unittest
+import requests
+
+baseUrl = "https://localhost:3000"
+
+def fixture_event():
+    return { "content" : "IBM" }
+
+class Test_DeleteBucketItem(unittest.TestCase):
+    def test_Delete_Bucket_return_success (self):
+        api_url = "http://localhost:3000/scraper/deleteBucketInput"
+        response = requests.post(api_url, json=fixture_event())
+        response.json()
+        print(response)
+
+        assert response.status_code == 200
+
+    def test_Delete_Bucket_Return_invalid_Input(self):
+        api_url = "http://localhost:3000/scraper/deleteBucketInput"
+        response = requests.post(api_url, json={})
+        response.json()
+        print(response)
+
+        assert response.status_code == 400
