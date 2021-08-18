c063f66f643d5af23ec546d23ee910dc9bf86f23
TylerArro <u18033882@tuks.co.za>
2021-08-18 21:14:24 +0200

    Updated app file for updateBucket

--- backend/ServerlessApplication/ptarmigan/scraper/UpdateBucket/app.py
+++ backend/ServerlessApplication/ptarmigan/scraper/UpdateBucket/app.py
@@ -1,8 +1,46 @@
 import json
 import boto3
 
-from ptarmigan.scraper.UpdateBucket.database import database
-from ptarmigan.scraper.UpdateBucket import database
+class database:
+    """This class will create a table if it exists, if not it will allow the user to add and remove from the db table"""
+    def __init__(self, companyName):
+        print("Company: !!!!!!!!", companyName)
+        self.tableName = companyName
+        self.dynamodbClient = boto3.client('dynamodb')
+        try:
+            response = self.dynamodbClient.create_table(
+                AttributeDefinitions=[
+                    {
+                        'AttributeName': 'Tweet_Id',
+                        'AttributeType': 'N',
+                    },
+                    {
+                        'AttributeName': 'TimeStamp',
+                        'AttributeType': 'N',
+                    }
+                ],
+                KeySchema=[
+                    {
+                        'AttributeName': 'Tweet_Id',
+                        'KeyType': 'HASH',
+                    },
+                    {
+                        'AttributeName': 'TimeStamp',
+                        'KeyType': 'RANGE',
+                    }
+                ],
+                ProvisionedThroughput={
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5,
+                },
+                TableName=self.tableName
+            )
+            #print("Table Did not exist")
+        except self.dynamodbClient.exceptions.ResourceInUseException:
+            #print("The DB Already exists")
+            # do something here as you require
+
+        #print("Something")
 
 
 def getBucketList():
@@ -104,3 +142,5 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps('Error updating Content file')
         }
+
+
