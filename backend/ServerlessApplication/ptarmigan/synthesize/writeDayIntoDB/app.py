a2a13f82504d77439b6896ce5aeef1c94368e4e8
Reinier <u18136835@tuks.co.za>
2021-08-23 21:40:34 +0200

    Added beginDate and end date to funciton call

---
+++ backend/ServerlessApplication/ptarmigan/synthesize/writeDayIntoDB/app.py
@@ -0,0 +1,41 @@
+import boto3
+import time
+from boto3.dynamodb.conditions import Key
+
+
+def calculateSentiment(content):
+    totalVotes = 0
+    runningSentiment = 0
+
+    for element in content:
+        weight = int(element["Weight"])
+        totalVotes = totalVotes + weight
+        sentiment = 0
+        if element["Sentiment"] == "NEUTRAL":
+            sentiment = 0
+        if element["Sentiment"] == "POSITIVE":
+            sentiment = 1
+        if element["Sentiment"] == "NEGATIVE":
+            sentiment = -1
+
+        runningSentiment = runningSentiment + (weight * sentiment)
+
+    if totalVotes == 0:
+        return 0
+    return (runningSentiment / totalVotes)
+
+def getAllFromDate(beginDate, endDate, companyName):
+    dynamodb = boto3.resource('dynamodb')
+    tableName = companyName
+    table = dynamodb.Table(tableName)
+
+    response = table.scan(
+        FilterExpression=Key('Tweet_Id').gt(1) & Key('TimeStamp').between(beginDate, endDate)
+    )
+
+    return calculateSentiment(response["Items"])
+
+
+def lambda_handler(event, context):
+
+    print (getAllFromDate(int(time.time())-86400, int(time.time()), "Tesla"))
