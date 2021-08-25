8de1758d1dbc9940118246f25d1ae540867f98a3
Reinier <u18136835@tuks.co.za>
2021-08-25 13:49:00 +0200

    added proper login

--- AdminFrontEnd/login.js
+++ AdminFrontEnd/login.js
@@ -1,5 +1,64 @@
+// import { CognitoIdentityClient, CreateIdentityPoolCommand } from "@aws-sdk/client-cognito-identity";
 var adminUsers
 let userpoolid = "eu-west-1_TWPQfb9SE"
+
+//=============== AWS IDs ===============
+var userPoolId = "eu-west-1_TWPQfb9SE";
+var clientId = '5ab6mv5p14eca01ci56f5up9tk';
+var region = 'eu-west-1';
+var identityPoolId = 'eu-west-1:236051c8-dfc0-423d-90b9-b06537c13935';
+//=============== AWS IDs ===============
+
+var cognitoUser;
+var idToken;
+var userPool;
+
+var poolData = {
+    UserPoolId : userPoolId,
+    ClientId : clientId
+};
+
+function logIn() {
+
+    if (!$('#loginUsername').val() || !$('#loginPassword').val()) {
+        logMessage('Please enter Username and Password!');
+    } else {
+        var authenticationData = {
+            Username: $('#loginUsername').val(),
+            Password: $("#loginPassword").val(),
+        };
+        var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+        userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+        var userData = {
+            Username: $('#loginUsername').val(),
+            Pool: userPool
+        };
+        cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+        // $("#loader").show();
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: function (result) {
+                // alert('Logged in!');
+                window.location.href = "index.html";
+                return
+                switchToLoggedInView();
+
+
+
+                idToken = result.getIdToken().getJwtToken();
+                getCognitoIdentityCredentials();
+
+
+            },
+
+            onFailure: function (err) {
+                alert(err.message);
+                // $("#loader").hide();
+            },
+
+        });
+    }
+}
 
 $(document).ready(function () {
 
@@ -35,21 +94,22 @@ $(document).ready(function () {
 
     });
     $('#loginButton').click(function () {
-        username = $("#loginUsername").val();
-        password = $("#loginPassword").val();
-
-
-        for (var i in adminUsers) {
-            for (var k in adminUsers[i].Attributes) {
-                if (adminUsers[i]["Attributes"][k]["Name"] == "email") {
-                    if (adminUsers[i]["Attributes"][k]["Value"] == username && password == "password") {
-                        window.location.href = "index.html";
-                        return
-                    }
-                }
-            }
-        }
-        alert("Incorrect username or password");
+        logIn()
+        // username = $("#loginUsername").val();
+        // password = $("#loginPassword").val();
+        //
+        //
+        // for (var i in adminUsers) {
+        //     for (var k in adminUsers[i].Attributes) {
+        //         if (adminUsers[i]["Attributes"][k]["Name"] == "email") {
+        //             if (adminUsers[i]["Attributes"][k]["Value"] == username && password == "password") {
+        //                 window.location.href = "index.html";
+        //                 return
+        //             }
+        //         }
+        //     }
+        // }
+        // alert("Incorrect username or password");
 
     });
 });
