aa54ba9c5cb85e5c6a4784fa695f9091830c861d
TylerArro <u18033882@tuks.co.za>
2021-08-27 20:57:30 +0200

    updated getStock sub function

--- backend/ServerlessApplication/ptarmigan/synthesize/writeDayIntoDB/app.py
+++ backend/ServerlessApplication/ptarmigan/synthesize/writeDayIntoDB/app.py
@@ -57,7 +57,7 @@ def writeIntoDb(date, company, stock, sentiment):
 def getStockPrice(date,ticker):
     formatDate = datetime.fromtimestamp(date)
     formatDate = formatDate.strftime("%Y-%m-%d")
-    requestUrl = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=VDLMI3ZNV3LSSLDZ"
+    requestUrl = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey=VDLMI3ZNV3LSSLDZ"
     requestReturn = requests.get(requestUrl)
     requestResults = json.loads(requestReturn.text)
     if not requestResults['Time Series (Daily)']:
@@ -84,7 +84,7 @@ def getTicker(company):
     requestReturn = json.loads(requestReturn.text)
     requestResults = requestReturn['results']
     if requestResults == None:
-        requestUrlCrypto = f"https://api.polygon.io/v3/reference/tickers?market=crypto&search={stuff} Dollar&order=asc&limit=10&apiKey=PNqoXU3luX7smsggLGPacHd8JnKZkDMV"
+        requestUrlCrypto = f"https://api.polygon.io/v3/reference/tickers?market=crypto&search={company} Dollar&order=asc&limit=10&apiKey=PNqoXU3luX7smsggLGPacHd8JnKZkDMV"
         requestReturnCrypto = requests.get(requestUrlCrypto)
         requestReturnCrypto = json.loads(requestReturnCrypto.text)
         requestResults = requestReturnCrypto['results']
