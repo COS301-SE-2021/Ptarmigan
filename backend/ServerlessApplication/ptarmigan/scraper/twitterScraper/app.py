19ea6319abe5d51b99d7953b7a0976ab9522b993
TylerArro <u18033882@tuks.co.za>
2021-09-03 15:31:35 +0200

    Updated to scrape on associated context and stagger by 3 hours

--- backend/ServerlessApplication/ptarmigan/scraper/twitterScraper/app.py
+++ backend/ServerlessApplication/ptarmigan/scraper/twitterScraper/app.py
@@ -3,37 +3,64 @@ import snscrape.modules.twitter
 import pandas as pd
 import datetime
 
-#helper function for handler
-def return_tweet_list(content,scrapeTimeframe):
+
+# helper function for handler
+def return_tweet_list(content, since, until):
     tweets_list = []
     # Using TwitterSearchScraper to scrape data and append tweets to list
     # ["ar", "hi", "ko", "zh-TW", "ja", "zh", "de", "pt", "en", "it", "fr", "es"]
     for i, tweet in enumerate(snscrape.modules.twitter.TwitterSearchScraper(
-            content + " min_faves:100" + " lang:ar OR lang:hi OR lang:ko OR lang:zh OR lang:ja OR lang:de OR lang:pt OR lang:en OR lang:it OR lang:fr OR lang:es since:" + scrapeTimeframe).get_items()):
+            content + " min_faves:100" + " lang:ar OR lang:hi OR lang:ko OR lang:zh OR lang:ja OR lang:de OR lang:pt OR lang:en OR lang:it OR lang:fr OR lang:es since_time:" + since + " until_time:" + until).get_items()):
         if i >= 100:
             break
         tweets_list.append([str(tweet.id), tweet.content, tweet.retweetCount,
                             tweet.likeCount, tweet.lang, tweet.date.timestamp()])
     # Creating a dataframe from the tweets list above
-        #print(type(tweets_list[0][0]))
+    # print(type(tweets_list[0][0]))
     return tweets_list
+
+
+def return_associated_list(content, since, until):
+    tweets_list = []
+    # Using TwitterSearchScraper to scrape data and append tweets to list
+    # ["ar", "hi", "ko", "zh-TW", "ja", "zh", "de", "pt", "en", "it", "fr", "es"]
+    for i, tweet in enumerate(snscrape.modules.twitter.TwitterSearchScraper(
+            content + " min_faves:100" + " lang:ar OR lang:hi OR lang:ko OR lang:zh OR lang:ja OR lang:de OR lang:pt OR lang:en OR lang:it OR lang:fr OR lang:es since_time:" + since + " until_time:" + until).get_items()):
+        if i >= 25:
+            break
+        tweets_list.append([str(tweet.id), tweet.content, tweet.retweetCount,
+                            tweet.likeCount, tweet.lang, tweet.date.timestamp()])
+    # Creating a dataframe from the tweets list above
+    # print(type(tweets_list[0][0]))
+    return tweets_list
+
 
 def scraper_handler(event, context):
     try:
         content = event['content']['content']
-        scrape_since = int(event['Scrape-until'])
+        scrapeTime = int(event['Scrape-until'])
     except:
         return {
             'statusCode': 400,
             'body': json.dumps("invalid input")
         }
-
-    scrape_timestamp = datetime.datetime.fromtimestamp(scrape_since)
-    scrape_dateformat = scrape_timestamp.strftime('%Y-%m-%d')
+    scrape_until = scrapeTime
+    scrape_since = scrapeTime - 10800
 
     # call helper function
-    tweets_list = return_tweet_list(content, scrape_dateformat)
-    #create dataframe
+    tweets_list = return_tweet_list(content, scrape_since, scrape_until)
+
+    if event['content']['Associated1']:
+        AssList1 = return_associated_list(content, scrape_since, scrape_until)
+        tweets_list.extend(AssList1)
+    if event['content']['Associated2']:
+        AssList2 = return_associated_list(content, scrape_since, scrape_until)
+        tweets_list.extend(AssList2)
+    if event['content']['Associated3']:
+        AssList3 = return_associated_list(content, scrape_since, scrape_until)
+        tweets_list.extend(AssList3)
+
+    # create dataframe
     tweets_df = pd.DataFrame(tweets_list,
                              columns=['Tweet Id', 'Text', 'Retweets', 'Likes', 'lang', 'date'])
     # Weight Calc
@@ -50,4 +77,4 @@ def scraper_handler(event, context):
     jsonOutput = tweets_df.to_json(orient='index')
     parsed = json.loads(jsonOutput)
     return parsed
-    #return json.dumps(parsed)
+    # return json.dumps(parsed)
