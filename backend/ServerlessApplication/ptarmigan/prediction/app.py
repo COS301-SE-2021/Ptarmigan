32bcc861d5e06609ea0d518eda6eabdb03ac52a7
TylerArro <u18033882@tuks.co.za>
2021-09-19 18:51:42 +0200

    updated sagemaker model

--- backend/ServerlessApplication/ptarmigan/prediction/app.py
+++ backend/ServerlessApplication/ptarmigan/prediction/app.py
@@ -7,7 +7,7 @@ import time
 from boto3.dynamodb.conditions import Key
 import yfinance as yf
 
-ENDPOINT_NAME = "sagemaker-scikit-learn-2021-09-18-19-35-22-824"
+ENDPOINT_NAME = "sagemaker-scikit-learn-2021-09-19-13-07-12-463"
 runtime = boto3.client('runtime.sagemaker')
 
 def lambda_handler(event, context):
