65830621b3b0854e3347e847dc42608e11633f86
TylerArro <u18033882@tuks.co.za>
2021-09-19 14:43:39 +0200

    write Lambda Handler function

--- backend/ServerlessApplication/ptarmigan/prediction/app.py
+++ backend/ServerlessApplication/ptarmigan/prediction/app.py
@@ -11,4 +11,18 @@ ENDPOINT_NAME = "sagemaker-scikit-learn-2021-09-18-19-35-22-824"
 runtime = boto3.client('runtime.sagemaker')
 
 def lambda_handler(event, context):
-    return "hello"
+    # get company name
+    try:
+        company = event['context']
+    except:
+        return {
+        'statusCode': 400,
+        'body': json.dumps("Invalid inputs")
+    }
+    dataArr = get_inputs(company)
+    #print(dataArr)
+    prediction = get_Prediction(dataArr)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(prediction)
+    }
