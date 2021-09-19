7771a46ae1b7f5cd1e118f29f3541aff85c0e1ec
TylerArro <u18033882@tuks.co.za>
2021-09-20 01:40:23 +0200

    fixed test

--- backend/ServerlessApplication/tests/integration/test_NonFunctionalReqs.py
+++ backend/ServerlessApplication/tests/integration/test_NonFunctionalReqs.py
@@ -12,7 +12,7 @@ class Test_Prediction(unittest.TestCase):
 
     def test_return_prediction_success(self):
         api_url = "https://cn9x0zd937.execute-api.eu-west-1.amazonaws.com/Prod/prediction"
-        response = requests.post(api_url, json=fixture_event)
+        response = requests.post(api_url, json=fixture_event())
         response.json()
         print(response)
 
