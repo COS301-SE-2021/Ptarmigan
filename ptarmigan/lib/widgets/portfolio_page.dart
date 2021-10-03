4d2d3873ca4bdc55a7314774943eced47ff13e3b
L_Bradford <u18185208@tuks.co.za>
2021-10-03 20:20:47 +0200

    Updated presentation and logic of Portfolio page.

--- ptarmigan/lib/widgets/portfolio_page.dart
+++ ptarmigan/lib/widgets/portfolio_page.dart
@@ -1,8 +1,13 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:ptarmigan/constants.dart';
 import 'package:ptarmigan/models/PortfolioItem.dart';
 import 'package:ptarmigan/services/portfolio_file_manager.dart';
+import 'package:ptarmigan/services/stock_price_generator.dart';
 import 'package:ptarmigan/widgets/home_page.dart';
+import 'package:ptarmigan/widgets/portfolio_history_screen.dart';
+
+import 'add_portfolio_page.dart';
 
 class PortfolioPage extends StatefulWidget {
   const PortfolioPage();
@@ -15,6 +20,7 @@ class _PortfolioPageState extends State<PortfolioPage> {
   Widget _body = Container(
       alignment: Alignment.center, child: CircularProgressIndicator());
   late PortfolioFileManager fileManager;
+  late Map portMap;
 
   _PortfolioPageState() {
     fileManager = new PortfolioFileManager();
@@ -32,8 +38,12 @@ class _PortfolioPageState extends State<PortfolioPage> {
           fileManager.getNameAndListMap().then((value) => {
                 setState(() {
                   print(value);
+                  portMap = value;
                   print("SetState called on portfolio page");
-                  _body = generatePortfolioWidgetList(value);
+                  if (value.isEmpty)
+                    _body = emptyFileWidget();
+                  else
+                    _body = generatePortfolioWidgetList(value);
                 })
               })
         });
@@ -53,11 +63,77 @@ class _PortfolioPageState extends State<PortfolioPage> {
     List keysList = portfolioMap.keys.toList();
     return SafeArea(
         child: Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DashboardScreen())),
+              ),
+            ),
             body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  for (var i in keysList) portfolioCard(i, portfolioMap[i]),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => PortfolioPage()));
+                            },
+                            child: Text(
+                              "Refresh",
+                              style: TextStyle(fontSize: 25),
+                            )),
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          AddPortfolioPage()));
+                            },
+                            child: Text(
+                              "Add item",
+                              style: TextStyle(fontSize: 25),
+                            )),
+                      ])
+                ],
+              ),
+            )));
+  }
+
+  Widget emptyFileWidget() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      alignment: Alignment.center,
       child: Column(
-        children: [for (var i in keysList) portfolioCard(i, portfolioMap[i])],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "No portfolio items have been added yet.",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AddPortfolioPage()));
+              },
+              child: Text(
+                "Add item.",
+                style: TextStyle(fontSize: 15),
+              ))
+        ],
       ),
-    )));
+    );
   }
 
   Widget portfolioCard(String name, List portfolioList) {
@@ -84,8 +160,11 @@ class _PortfolioPageState extends State<PortfolioPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              decoration:
-                  BoxDecoration(border: Border.all(color: Colors.white)),
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.lightBlueAccent, width: 2),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
               child: Text(name, style: TextStyle(fontSize: 30)),
             ),
             Row(
@@ -96,9 +175,13 @@ class _PortfolioPageState extends State<PortfolioPage> {
                   style: TextStyle(fontSize: 20),
                 ),
                 Container(
+                  margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
                   height: 30,
                   width: 100,
-                  child: Text(portfolioItem.amountOwned.toString()),
+                  child: Text(
+                    portfolioItem.amountOwned.toString(),
+                    style: TextStyle(fontSize: 20),
+                  ),
                 )
               ],
             ),
@@ -107,18 +190,27 @@ class _PortfolioPageState extends State<PortfolioPage> {
               children: [
                 Text("Last stock price : ", style: TextStyle(fontSize: 20)),
                 Container(
+                  margin: EdgeInsets.fromLTRB(0, 20, 0, 10),
                   height: 30,
                   width: 100,
-                  child: Text(portfolioItem.currentStockValue.toString()),
+                  child: Text(portfolioItem.currentStockValue.toString(),
+                      style: TextStyle(fontSize: 20)),
                 )
               ],
             ),
             Container(
+              decoration: BoxDecoration(
+                  border:
+                      Border(top: BorderSide(width: 1, color: Colors.white))),
+              margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
               height: 60,
               width: 200,
-              child: Text(DateTime.fromMillisecondsSinceEpoch(
-                      portfolioItem.timeStamp.round())
-                  .toString()),
+              child: Text(
+                  DateTime.fromMillisecondsSinceEpoch(
+                          portfolioItem.timeStamp.round())
+                      .toString(),
+                  style: TextStyle(fontSize: 15)),
             ),
             Container(
               child: Row(
@@ -144,22 +236,32 @@ class _PortfolioPageState extends State<PortfolioPage> {
             ),
             Container(
               height: 30,
-            )
+            ),
           ],
         ));
   }
 
   void update(PortfolioItem item) {
+    StockPriceGenerator generator = new StockPriceGenerator();
     PortfolioItem tempItem = new PortfolioItem();
     tempItem.stockName = item.stockName;
     tempItem.amountOwned = item.amountOwned;
-    //tempItem.currentStockValue
-    tempItem.timeStamp = DateTime.now().millisecondsSinceEpoch;
 
-    fileManager.addIntoStockList(tempItem);
+    tempItem.timeStamp = DateTime.now().millisecondsSinceEpoch;
+    generator.fetchPrices().then((value) => {
+          tempItem.currentStockValue = value[tempItem.stockName],
+          print("tempItem = " + tempItem.toStringWithName()),
+          fileManager.addIntoStockList(tempItem)
+        });
   }
 
-  void history(PortfolioItem item) {}
+  void history(PortfolioItem item) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>
+                PortfolioHistoryScreen(item.stockName, portMap)));
+  }
 
   void delete(PortfolioItem item) {
     fileManager.removePortfolio(item.stockName);
